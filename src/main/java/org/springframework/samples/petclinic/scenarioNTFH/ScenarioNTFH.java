daa9aa779e2737d95a5e2d4b0ac47a73f5e0edfb
alegestor <alegestor@alum.us.es>
2021-10-27 20:33:51 +0200

    Intento de entidad

--- src/main/java/org/springframework/samples/petclinic/scenarioNTFH/ScenarioNTFH.java
+++ src/main/java/org/springframework/samples/petclinic/scenarioNTFH/ScenarioNTFH.java
@@ -1,6 +1,8 @@
-package org.springframework.samples.petclinic.model;
+package org.springframework.samples.petclinic.scenarioNTFH;
 
 import javax.persistence.Entity;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
 
 import lombok.Data;
 
