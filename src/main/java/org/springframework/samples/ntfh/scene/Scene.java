48da06194e4e8815154d564307afe9205efb6d16
andrsdt <andresdt@pm.me>
2021-11-14 15:15:17 +0100

    feat: in Scene,  replace String with SceneType

--- src/main/java/org/springframework/samples/ntfh/scene/Scene.java
+++ src/main/java/org/springframework/samples/ntfh/scene/Scene.java
@@ -1,17 +1,21 @@
 package org.springframework.samples.ntfh.scene;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
-import org.springframework.samples.ntfh.model.NamedEntity;
+import org.springframework.samples.ntfh.enumerates.SceneType;
+import org.springframework.samples.ntfh.model.BaseEntity;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "scenes")
-public class Scene extends NamedEntity {
-    private String modifier;
+public class Scene extends BaseEntity {
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private SceneType type;
 }
