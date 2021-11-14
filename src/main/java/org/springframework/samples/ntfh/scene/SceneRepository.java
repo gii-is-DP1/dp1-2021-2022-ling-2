3da4a1e8857350b003fb8abaef8b00ecbdb7ce5b
andrsdt <andresdt@pm.me>
2021-11-14 02:38:13 +0100

    refactor: remove spaces

--- src/main/java/org/springframework/samples/ntfh/scene/SceneRepository.java
+++ src/main/java/org/springframework/samples/ntfh/scene/SceneRepository.java
@@ -1,9 +1,7 @@
-
-
 package org.springframework.samples.ntfh.scene;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface SceneRepository extends CrudRepository<Scene, Integer> {
-    
+
 }
