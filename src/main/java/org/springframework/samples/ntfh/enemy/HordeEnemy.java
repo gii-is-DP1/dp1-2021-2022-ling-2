d49dd21a3cd03e783a8b632b0bed6c2da708ab53
andrsdt <andresdt@pm.me>
2021-11-14 13:55:11 +0100

    add!: big changes in Enemy and related entities

--- src/main/java/org/springframework/samples/ntfh/enemy/HordeEnemy.java
+++ src/main/java/org/springframework/samples/ntfh/enemy/HordeEnemy.java
@@ -1,28 +1,97 @@
 package org.springframework.samples.ntfh.enemy;
 
+import java.util.Map;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
+import org.springframework.samples.ntfh.enumerates.HordeEnemyType;
 import org.springframework.samples.ntfh.enumerates.HordeModifier;
+import org.springframework.samples.ntfh.model.BaseEntity;
 
 import lombok.Getter;
-import lombok.Setter;
 
+// No getters needed? These entities will stay the same
 @Getter
-@Setter
 @Entity
 @Table(name = "horde_enemies")
-public class HordeEnemy extends Enemy {
-    @NotNull
-    private Integer glory;
-
+public class HordeEnemy extends BaseEntity implements Enemy {
+    @Column(name = "extra_glory", columnDefinition = "integer default 0")
     private Integer extraGlory;
 
+    @Column(columnDefinition = "integer default 0")
     private Integer gold;
 
     @Enumerated(EnumType.STRING)
-    private HordeModifier modifier;
+    private HordeEnemyType type;
+
+    /**
+     * Return the HordeModifier of an enemy, or null if it doesn't have any. Derived
+     * from HordeEnemyType.
+     * 
+     * @author andrsdt
+     * @return HordeModifier enum value of the HordeEnemy
+     */
+    public HordeModifier getHordeModifier() {
+        // Derived property,
+        HordeModifier modifier = null;
+        if (type == HordeEnemyType.SHAMAN || type == HordeEnemyType.MAGE) {
+            modifier = HordeModifier.MAGIC_ATTACKER;
+        } else if (type == HordeEnemyType.REGEN) {
+            modifier = HordeModifier.HEALING_CAPABILITIES;
+        }
+        return modifier;
+    }
+
+    /**
+     * Return the endurance of an enemy computed by knowing its HordeEnemyType.
+     * 
+     * @author andrsdt
+     * @return Integer value of the HordeEnemy's endurance
+     */
+    @Override
+    public Integer getEndurance() {
+        Map<HordeEnemyType, Integer> enemyEndurance = Map.of(HordeEnemyType.SLINGER, 2, HordeEnemyType.REGEN, 3,
+                HordeEnemyType.SHAMAN, 3, HordeEnemyType.WARRIOR, 4, HordeEnemyType.MAGE, 5, HordeEnemyType.BERSERKER,
+                6);
+
+        return enemyEndurance.get(type);
+    }
+
+    /**
+     * Return the route to the image of an enemy. Can be obtained by knowing the
+     * HordeEnemyType and, in case of the warrior, also the loot behind the card
+     * 
+     * @author andrsdt
+     * @return route to the card image
+     */
+    @Override
+    public String getFrontImage() {
+        // Derived property. Can be obtained by knowing the EnemyType, and in some
+        // specific cases the extraGlory and gold
+
+        // WarlordType.GURDRUG -> gurdrug
+        String hordeEnemyName = type.toString().toLowerCase();
+
+        // The warrior with extraGlory and gold has a different image
+        if ((type == HordeEnemyType.WARRIOR) && (extraGlory != null || gold != null))
+            hordeEnemyName = "warrior_with_loot";
+
+        return "/cards/enemies/horde_enemies/fronts/" + hordeEnemyName + ".png";
+    }
+
+    /**
+     * Return the route to the standard back image of a HordeEnemy.
+     * 
+     * @author andrsdt
+     * @return String route to the card's back image
+     */
+    @Override
+    public String getBackImage() {
+        return String.format("/cards/enemies/horde_enemies/backs/%dgold_%dglory.png", gold, extraGlory);
+    }
+
 }
--- src/main/java/org/springframework/samples/ntfh/enemy/Warlord.java
+++ src/main/java/org/springframework/samples/ntfh/enemy/Warlord.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.ntfh.enemy;
 
+import java.util.Map;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -7,6 +9,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.samples.ntfh.enumerates.WarlordType;
+import org.springframework.samples.ntfh.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,9 +18,46 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "warlords")
-public class Warlord extends Enemy {
+public class Warlord extends BaseEntity implements Enemy {
 
     @NotNull
     @Enumerated(EnumType.STRING)
     private WarlordType type;
+
+    /**
+     * Return the endurance of a warlord. Can be obtained by knowing the WarlordType
+     * 
+     * @author andrsdt
+     * @return Integer value of the warlord's endurance
+     */
+    @Override
+    public Integer getEndurance() {
+        Map<WarlordType, Integer> warlordEndurance = Map.of(WarlordType.GURDRUG, 8, WarlordType.ROGHKILLER, 9,
+                WarlordType.SHRIEKKNIFER, 10);
+
+        return warlordEndurance.get(type);
+    }
+
+    /**
+     * Return the route to the front image of a warlord. Can be obtained by knowing
+     * the WarlordType
+     * 
+     * @author andrsdt
+     * @return String route to the card's front image
+     */
+    @Override
+    public String getFrontImage() {
+        return "/cards/enemies/warlords/" + type.toString().toLowerCase() + ".png";
+    }
+
+    /**
+     * Return the route to the standard back image of a warlord.
+     * 
+     * @author andrsdt
+     * @return String route to the card's back image
+     */
+    @Override
+    public String getBackImage() {
+        return "/cards/enemies/warlords/backs/standard.png";
+    }
 }
