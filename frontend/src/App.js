1f4d0aa78618e8786071ceb1acd3885bd181e59a
andrsdt <andresdt@pm.me>
2021-11-17 14:14:46 +0100

    feat: improve basic frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
+import NotFound from "./pages/not-found";
 import * as ROUTES from "./constants/routes";
 import "./App.css";
 
@@ -12,7 +13,7 @@ export default function App() {
         <Route exact path={ROUTES.LOGIN} component={Login} />
         <Route exact path={ROUTES.SIGNUP} component={SignUp} />
         <Route exact path={ROUTES.HOME} component={Home} />
-        {/* <Route component={NotFound} /> */}
+        <Route component={NotFound} />
         {/* All routes have "exact" since that means that the path has to match the exact string. If a route is not correct, the fallback is ROUTES.NOT_FOUND since it's the only one without "exact" */}
       </Switch>
     </Router>
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,8 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
 
 export default function SignUp() {
   useEffect(() => {
     document.title = "NTFH - Sign up";
   });
-  return <h1>Sign up page</h1>;
+
+  const [username, setUsername] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  return (
+    <div>
+      <h1>Sign up page</h1>
+      <p>
+        {" "}
+        Have an account?
+        <Link to={ROUTES.LOGIN}>Log in</Link>
+      </p>
+    </div>
+  );
 }
