0b640e65c17be7527d639b400d1f273a48fb33a2
andrsdt <andresdt@pm.me>
2021-11-17 10:47:23 +0100

    add: dependencies and base structure

--- frontend.bak/src/App.js
+++ frontend.bak/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route exact path={ROUTES.LOGIN} component={Login} />
         <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
         <Route exact path={ROUTES.HOME} component={Home} />
         <Route component={NotFound} />
         {/* All routes have "exact" since that means that the path has to match the exact string. If a route is not correct, the fallback is ROUTES.NOT_FOUND since it's the only one without "exact" */}
-      </Routes>
+      </Switch>
     </Router>
   );
 }
