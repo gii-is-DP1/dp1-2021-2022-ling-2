ec979a99fd35e0bc0a7bb120ce8fd46af6dac704
andrsdt <andresdt@pm.me>
2021-11-18 10:19:56 +0100

    add: custom error handler

--- src/main/java/org/springframework/samples/ntfh/configuration/ExceptionHandlerConfiguration.java
+++ src/main/java/org/springframework/samples/ntfh/configuration/ExceptionHandlerConfiguration.java
@@ -4,6 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,13 +21,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlerConfiguration {
     @Autowired
     private BasicErrorController errorController;
-    // add any exceptions/validations/binding problems
 
-    @ExceptionHandler(Exception.class)
-    public String defaultErrorHandler(HttpServletRequest request, Exception ex) {
-        request.setAttribute("javax.servlet.error.request_uri", request.getPathInfo());
-        request.setAttribute("javax.servlet.error.status_code", 400);
-        request.setAttribute("exeption", ex);
-        return "exception";
+    // add any exceptions/validations/binding problems
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> dataAccessExceptionHandler(HttpServletRequest request, DataAccessException ex) {
+        // TODO return a proper error response in JSON format so the frontend can handle
+        // it easily
+        return new ResponseEntity<>("The data was not found", HttpStatus.NOT_FOUND);
     }
 }
