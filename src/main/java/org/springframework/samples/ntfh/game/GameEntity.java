f040cbf72e28a8aca230de1915e93f323dd19936
andrsdt <andresdt@pm.me>
2021-11-19 10:20:24 +0100

    feat: andres changes

--- src/main/java/org/springframework/samples/ntfh/game/GameEntity.java
+++ src/main/java/org/springframework/samples/ntfh/game/GameEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
-
+import org.springframework.data.annotation.Transient;
 import org.springframework.samples.ntfh.comments.Comment;
 import org.springframework.samples.ntfh.model.NamedEntity;
 
@@ -23,13 +23,25 @@ public class GameEntity extends NamedEntity {
     private Timestamp finishTime;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "game")
     private Set<Comment> comments;
-    //Lista de usuarios en el juego [Mirar!!!!!!!]
-    //@OneToMany(cascade = CascadeType.ALL,mappedBy = "game")
-    //private Set<User> players;
+    // Lista de usuarios en el juego [Mirar!!!!!!!]
+    // @OneToMany(cascade = CascadeType.ALL,mappedBy = "game")
+    // private Set<User> players;
+
     private Boolean spectatorsAllowed;
 
+    private Boolean scenariosAllowed;
+
+    // TODO tiene que ser null si !scenariosallowed
+    // Esto hace referencia a una tabla donde estan todos los escenarios
+    private SceneType currentScene;
+
+    @Transient // Indica que es un getter, no queremos que se guarde en la tabla
     public Long getDuration() {
         return finishTime.getTimestamp().getTime() - startTime.getTimestamp().getTime();
     }
 
+    @Transient
+    public boolean isFinished() {
+        return finishTime != null;
+    }
 }
