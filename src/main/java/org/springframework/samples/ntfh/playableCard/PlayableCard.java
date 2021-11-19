b81025bd2af455bb7d49a86f466135d6f59dcf65
andrsdt <andresdt@pm.me>
2021-11-20 00:28:23 +0100

    refactor: pasando el problema a otro

--- src/main/java/org/springframework/samples/ntfh/playableCard/PlayableCard.java
+++ src/main/java/org/springframework/samples/ntfh/playableCard/PlayableCard.java
@@ -5,53 +5,52 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
-import org.springframework.samples.ntfh.character.CharacterType;
-import org.springframework.samples.ntfh.interfaces.AbilityCard;
+import org.springframework.samples.ntfh.character.CharacterTypeEnum;
 import org.springframework.samples.ntfh.interfaces.Location;
 import org.springframework.samples.ntfh.interfaces.PhysicalCard;
-import org.springframework.samples.ntfh.interfaces.PlayableCardType;
-import org.springframework.samples.ntfh.marketCard.MarketCardEnum;
 import org.springframework.samples.ntfh.model.BaseEntity;
+
 import lombok.Getter;
 
+// TODO we have to change the implementation of this class or remove it
 @Getter
 @Entity
 @Table(name = "playable_cards")
-public class PlayableCard extends BaseEntity implements PhysicalCard {
+public class PlayableCard extends BaseEntity {
 
-    // TODO esta propiedad no debería estar aquí. Tenemos que hacer una entidad nueva que sea de instancias de
-    // cartas ingame y ahí será donde la carta tenga una posición, un poseedor... Aquí solo se guardan propiedades
-    // generales de la carta, comunes a todas las partidas, y a partir de aquí se crearán las instancias específicas
-    // de las cartas para cada partida
+    // TODO esta propiedad no debería estar aquí. Tenemos que hacer una entidad
+    // nueva que sea de instancias de cartas ingame y ahí será donde la carta tenga
+    // una posición, un poseedor... Aquí solo se guardan propiedades generales de la
+    // carta, comunes a todas las partidas, y a partir de aquí se crearán las
+    // instancias específicas de las cartas para cada partida
+
     @Enumerated(EnumType.STRING)
     private PlayableCardLocation location;
 
- //   @Enumerated(EnumType.STRING)
- //   private AbilityCard cardType; // Enum que englobe las AbilityCard de todos los personajes
-    
+    // @Enumerated(EnumType.STRING)
+    // private AbilityCard cardType; // Enum que englobe las AbilityCard de todos
+    // los personajes
+
     // TODO refactorizar
     public String switchCharacter(String character) {
-        if(character.matches(CharacterType.RANGER.toString())) return RangerCard.values().toString();
-        else if (character.matches(CharacterType.ROGUE.toString())) return RogueCard.values().toString();
-        else if (character.matches(CharacterType.WARRIOR.toString())) return WarriorCard.values().toString();
-        else if (character.matches(CharacterType.WIZARD.toString())) return WizardCard.values().toString();
-        else return null;
+        if (character.matches(CharacterTypeEnum.RANGER.toString()))
+            return RangerCard.values().toString();
+        else if (character.matches(CharacterTypeEnum.ROGUE.toString()))
+            return RogueCard.values().toString();
+        else if (character.matches(CharacterTypeEnum.WARRIOR.toString()))
+            return WarriorCard.values().toString();
+        else if (character.matches(CharacterTypeEnum.WIZARD.toString()))
+            return WizardCard.values().toString();
+        else
+            return null;
     }
 
-    @Override
     public String getFrontImage() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
     public String getBackImage() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Location getLocation() {
         // TODO Auto-generated method stub
         return null;
     }
