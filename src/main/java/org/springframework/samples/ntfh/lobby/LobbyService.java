37430df71cb895ef9b1075d08e5cd16428ab6cb1
andrsdt <andresdt@pm.me>
2021-11-21 10:49:10 +0100

    add: restricted lobby service method

--- src/main/java/org/springframework/samples/ntfh/lobby/LobbyService.java
+++ src/main/java/org/springframework/samples/ntfh/lobby/LobbyService.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.ntfh.lobby;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -30,6 +32,40 @@ public class LobbyService {
     @Transactional
     public Optional<Lobby> findLobbyById(int id) {
         return lobbyRepository.findById(id);
+    }
+
+    /**
+     * 
+     * @param id of the lobby to be fetched
+     * @return Lobby object without sensitive information (users' passwords)
+     */
+    @Transactional
+    public Optional<Lobby> findLobbyByIdNonSensitive(int id) {
+        // TODO unused. Maybe replace with a custom JSON parser?
+        Optional<Lobby> lobbyOptional = lobbyRepository.findById(id);
+        if (!lobbyOptional.isPresent())
+            return lobbyOptional;
+        Lobby lobby = lobbyOptional.get();
+
+        // Create a Lobby object without sensitive information
+        Lobby lobbyNonSensitive = new Lobby();
+        lobbyNonSensitive.setId(lobby.getId());
+        lobbyNonSensitive.setName(lobby.getName());
+        lobbyNonSensitive.setHasStarted(lobby.getHasStarted());
+        lobbyNonSensitive.setHasScenes(lobby.getHasScenes());
+        lobbyNonSensitive.setSpectatorsAllowed(lobby.getSpectatorsAllowed());
+        lobbyNonSensitive.setMaxPlayers(lobby.getMaxPlayers());
+        Set<User> usersNonSensitive = new HashSet<>();
+        Set<User> lobbyUsers = lobbyNonSensitive.getUsers();
+        if (lobbyUsers != null) {
+            lobbyUsers.forEach(user -> {
+                User userNonSensitive = new User();
+                userNonSensitive.setUsername(user.getUsername());
+                usersNonSensitive.add(userNonSensitive);
+            });
+            lobbyNonSensitive.setUsers(usersNonSensitive);
+        }
+        return Optional.of(lobbyNonSensitive); // assure that it always returns an optional
     }
 
     @Transactional
