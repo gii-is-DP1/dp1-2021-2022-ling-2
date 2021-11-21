d89d064ae021404d6a3856012f9976886fc4bdfd
andrsdt <andresdt@pm.me>
2021-11-22 00:20:54 +0100

    add: small improvements in sidebar style

--- react/src/components/home/Sidebar.js
+++ react/src/components/home/Sidebar.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import UserContext from "../../context/user";
 import { Button } from "react-bootstrap";
 import tokenParser from "../../helpers/tokenParser";
@@ -9,17 +9,24 @@ import * as ROUTES from "../../constants/routes";
  * @returns {React.Component}
  */
 export default function Sidebar() {
+  const history = useHistory();
   const { setUserToken } = useContext(UserContext);
   const user = tokenParser(useContext(UserContext));
+
+  const handleLogout = () => {
+    setUserToken(null);
+    history.push(ROUTES.HOME);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-dark bg-dark">
       <h1 className="text-white">Welcome back, {user.username}</h1>
       <Link to={ROUTES.PROFILE.replace(":username", user.username)}>
         <Button className="m-2" type="submit">
           Profile
         </Button>
       </Link>
-      <Button className="m-2" type="submit" onClick={() => setUserToken(null)}>
+      <Button className="m-2" type="submit" onClick={() => handleLogout()}>
         Log out
       </Button>
     </nav>
