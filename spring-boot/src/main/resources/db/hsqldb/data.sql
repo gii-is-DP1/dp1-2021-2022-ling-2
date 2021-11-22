abd0bc83126985d3e3b4d3949443021d89f03129
Andrés <andresdt@pm.me>
2021-11-22 14:21:03 +0100

    fix: accidental repetition in data.sql

--- spring-boot/src/main/resources/db/hsqldb/data.sql
+++ spring-boot/src/main/resources/db/hsqldb/data.sql
@@ -6,10 +6,7 @@ INSERT INTO authorities(id,username,authority) VALUES (2,'admin','user');
 INSERT INTO users(username,password,email) VALUES ('andres','andres','andres@mail.com');
 INSERT INTO authorities(id,username,authority) VALUES (3,'andres','user');
 
-INSERT INTO lobbies(name, has_started, has_scenes, spectators_allowed, max_players) VALUES ('test lobby 1', false, false, true, 2);
-INSERT INTO lobbies(name, has_started, has_scenes, spectators_allowed, max_players) VALUES ('test lobby 2', false, true, false, 3);
-INSERT INTO lobbies(name, has_started, has_scenes, spectators_allowed, max_players) VALUES ('test lobby 3', false, true, true, 4);
-
+-- TODO missing host?
 INSERT INTO lobbies(name, has_started, has_scenes, spectators_allowed, max_players) VALUES ('test lobby 1', false, false, true, 2);
 INSERT INTO lobbies(name, has_started, has_scenes, spectators_allowed, max_players) VALUES ('test lobby 2', false, true, false, 3);
 INSERT INTO lobbies(name, has_started, has_scenes, spectators_allowed, max_players) VALUES ('test lobby 3', false, true, true, 4);
