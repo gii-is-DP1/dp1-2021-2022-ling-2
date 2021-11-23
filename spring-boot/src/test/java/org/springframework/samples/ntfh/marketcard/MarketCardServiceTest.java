d95350e0a47861d0b8fd227c3391c7734f6eb5ff
alegestor <alegestor@alum.us.es>
2021-11-23 13:29:10 +0100

    .

--- spring-boot/src/test/java/org/springframework/samples/ntfh/marketcard/MarketCardServiceTest.java
+++ spring-boot/src/test/java/org/springframework/samples/ntfh/marketcard/MarketCardServiceTest.java
@@ -1,4 +1,4 @@
-package org.springframework.samples.ntfh.marketcard;
+package org.springframework.samples.ntfh.marketCard;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +12,9 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.ntfh.marketcard.MarketCard;
+import org.springframework.samples.ntfh.marketcard.MarketCardService;
+import org.springframework.samples.ntfh.marketcard.MarketCardTypeEnum;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
