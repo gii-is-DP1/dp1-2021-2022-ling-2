c80766390d39040f0234bfcb08278bf27e551ecd
andrsdt <andresdt@pm.me>
2021-11-23 16:29:07 +0100

    refactor: remove unused imports

--- spring-boot/src/main/java/org/springframework/samples/ntfh/game/Game.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/game/Game.java
@@ -2,17 +2,12 @@ package org.springframework.samples.ntfh.game;
 
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import org.springframework.data.annotation.Transient;
-import org.springframework.samples.ntfh.comments.Comment;
 import org.springframework.samples.ntfh.model.BaseEntity;
 import org.springframework.samples.ntfh.player.Player;
 
@@ -42,12 +37,4 @@ public class Game extends BaseEntity {
 
     @OneToOne
     private Player leader;
-
-    // TODO a good idea would be to have a GameHistory entity where finished games
-    // are stored. This way, this class will only be used to refer to the game a
-    // User is currently playing in, and the GameHistory will be an immutable table
-    // whose rows are created with the final results of a game (including players
-    // who were playing on it, finish time and many others that will only matter
-    // once the game has finished)
-
 }
