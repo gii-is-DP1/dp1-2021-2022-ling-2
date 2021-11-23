03e3cb22a3e97406d5bed9d6e33df4d679858b7c
andrsdt <andresdt@pm.me>
2021-11-23 21:30:50 +0100

    fix: User JSON deserializer missing values

--- spring-boot/src/main/java/org/springframework/samples/ntfh/user/UserDeserializer.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/user/UserDeserializer.java
@@ -43,7 +43,26 @@ public class UserDeserializer extends JsonDeserializer<User> {
                 throw new DataAccessException("User not found") {
                 };
 
-            return optionalUserFromUsername.get();
+            // TODO consider using the Prototype pattern for creating a user clone
+            User userFromDb = optionalUserFromUsername.get();
+            User user = new User();
+            user.setAuthorities(userFromDb.getAuthorities());
+            user.setEnabled(node.get("enabled") == null ? userFromDb.isEnabled() : node.get("enabled").asBoolean());
+            user.setPassword(node.get("password") == null ? userFromDb.getPassword() : node.get("password").asText());
+            user.setUsername(node.get("username") == null ? userFromDb.getUsername() : node.get("username").asText());
+            user.setEmail(node.get("email") == null ? userFromDb.getEmail() : node.get("email").asText());
+
+            // TODO possible point of failure. Currently, if we pass these values as
+            // parameters in JSON they won't be considered and their value will be set to
+            // the current value on the database. This is due to the effort in parsing
+            // custom classes. In case we need this at some point, we would implement
+            // parsers that creates objects from the JSON and then sets the values on the
+            // object.
+            user.setCharacter(userFromDb.getCharacter());
+            user.setGame(userFromDb.getGame());
+            user.setLobby(userFromDb.getLobby());
+
+            return user;
         } catch (Exception e) {
             throw new IOException(e);
         }
