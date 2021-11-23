8f39e5f3a74fa5a0a98759de23f40398c7654244
andrsdt <andresdt@pm.me>
2021-11-23 21:32:25 +0100

    refactor: small changes

--- react/src/pages/admin-page.js
+++ react/src/pages/admin-page.js
@@ -6,121 +6,126 @@ import UserContext from "../context/user";
 import Homebar from "../components/home/Homebar";
 
 export default function AdminPage() {
-    const history = useHistory();
-    const { userToken } = useContext(UserContext);
-    const [gameList, setGameList] = useState([]);
-    const [userList, setUserList] = useState([]);
-    const [errors, setErrors] = useState([]);
+  const history = useHistory();
+  const { userToken } = useContext(UserContext);
+  const [gameList, setGameList] = useState([]);
+  const [userList, setUserList] = useState([]);
+  const [errors, setErrors] = useState([]);
 
-    useEffect(() => {
-        const fetchGames = async() => {
-            try{
-                const response = await axios.get(`games`);
-                setGameList(response.data);
-            } catch(error) {
-                setErrors([...errors, error.message]);
-            }
-        }
+  useEffect(() => {
+    // TODO make these calls when clicking on the corresponding button
+    const fetchGames = async () => {
+      try {
+        const response = await axios.get(`games`);
+        setGameList(response.data);
+      } catch (error) {
+        setErrors([...errors, error.message]);
+      }
+    };
+    // TODO make these calls when clicking on the corresponding button
+    const fetchUsers = async () => {
+      try {
+        const headers = { Authorization: "Bearer " + userToken };
+        const response = await axios.get(`users`, { headers });
+        setUserList(response.data);
+      } catch (error) {
+        setErrors([...errors, error.message]);
+      }
+    };
 
-        const fetchUsers = async() => {
-            try{
-                const headers = { Authorization: "Bearer " + userToken }
-                const response = await axios.get(`users`, {headers});
-                setUserList(response.data);
-            } catch(error) {
-                setErrors([...errors, error.message]);
-            }
-        }
+    fetchUsers();
+  }, []);
 
-        fetchUsers();
-    }, []);
+  return (
+    <div>
+      <Homebar />
+      <Container>
+        <Row>
+          {/* Buttons Column */}
+          <Col>
+            <Row>
+              <Button type="submit" onClick={() => ""}>
+                Games
+              </Button>
+            </Row>
+            <Row>
+              <Button type="submit" onClick={() => ""}>
+                History
+              </Button>
+            </Row>
+            <Row>
+              <Button type="submit" onClick={() => ""}>
+                Users
+              </Button>
+            </Row>
+            <Row>
+              <Button type="submit" onClick={() => ""}>
+                Achievements
+              </Button>
+            </Row>
+          </Col>
 
-    return (
-        <div>
-            <Homebar />
-            <Container>
-                <Row>
-                    {/* Buttons Column */}
-                    <Col>
-                        <Row>
-                            <Button type="submit" onClick={() => ""}>
-                                Games
-                            </Button>
-                        </Row>
-                        <Row>
-                            <Button type="submit" onClick={() => ""}>
-                                History
-                            </Button>
-                        </Row>
-                        <Row>
-                            <Button type="submit" onClick={() => ""}>
-                                Users
-                            </Button>
-                        </Row>
-                        <Row>
-                            <Button type="submit" onClick={() => ""}>
-                                Achievements
-                            </Button>
-                        </Row>
-                    </Col>
+          {/* Data Column */}
+          <Col>
+            {/* Game Table */}
+            <Table>
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>name</th>
+                  <th>duration</th>
+                  <th>start_time</th>
+                  <th>finish_time</th>
+                  <th>has_finished</th>
+                  <th>has_scenes</th>
+                </tr>
+              </thead>
+              <tbody>
+                {gameList.map((game, idx) => (
+                  <tr>
+                    <th>game.id</th>
+                    <th>game.name</th>
+                    <th>game.duration</th>
+                    <th>game.start_time</th>
+                    <th>game.finish_time</th>
+                    <th>game.has_finished</th>
+                    <th>game.has_scenes</th>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
 
-                    {/* Data Column */}
-                    <Col>
-                    {/* Game Table */}
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>id</th>
-                                <th>name</th>
-                                <th>duration</th>
-                                <th>start_time</th>
-                                <th>finish_time</th>
-                                <th>has_finished</th>
-                                <th>has_scenes</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {gameList.map((game, idx) => (
-                            <tr>
-                                <th>game.id</th>
-                                <th>game.name</th>
-                                <th>game.duration</th>
-                                <th>game.start_time</th>
-                                <th>game.finish_time</th>
-                                <th>game.has_finished</th>
-                                <th>game.has_scenes</th>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
-
-                    {/* Users Table */}
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>username</th>
-                                <th>email</th>
-                                <th>enabled</th>
-                                <th>modify</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {userList.map((user, idx) => (
-                            <tr>
-                                <th>{user.username}</th>
-                                <th>{user.email}</th>
-                                <th>{user.enabled ? '✓' : 'X'}</th>
-                                <Button variant="primary" onClick={() =>
-                                    history.push(`/profile/${user.username}/edit`)}>
-                                    Edit profile
-                                </Button>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+            {/* Users Table */}
+            <Table>
+              <thead>
+                <tr>
+                  <th>username</th>
+                  <th>email</th>
+                  <th>enabled</th>
+                  <th>modify</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userList.map((user, idx) => (
+                  <tr>
+                    <th>{user.username}</th>
+                    <th>{user.email}</th>
+                    <th>{user.enabled ? "🟢" : "🔴"}</th>
+                    <Button
+                      variant="primary"
+                      onClick={() =>
+                        history.push(`/profile/${user.username}/edit`)
+                      }
+                    >
+                      Edit profile
+                    </Button>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
