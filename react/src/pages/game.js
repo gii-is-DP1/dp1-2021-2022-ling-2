74765ef3873606902766a16e678e0c5c4859874e
andrsdt <andresdt@pm.me>
2021-11-25 19:58:43 +0100

    add: frontend fetches game info, shows if user is spectator

--- react/src/pages/game.js
+++ react/src/pages/game.js
@@ -1,9 +1,52 @@
-import axios from "../api/axiosConfig";
+import { useContext } from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import UserContext from "../context/user";
+import ErrorContext from "../context/error";
+import SpectatorToast from "../components/game/SpectatorToast";
+import axios from "../api/axiosConfig";
+import tokenParser from "../helpers/tokenParser";
 
 export default function Game() {
   // route for /games/gameId
   const { gameId } = useParams(); // get params from react router link
-  return <h1>This is game {gameId}</h1>;
+  const { userToken } = useContext(UserContext);
+  const loggedUser = tokenParser(useContext(UserContext));
+  const [game, setGame] = useState(null);
+  const [user, setUser] = useState(null);
+  const { errors, setErrors } = useContext(ErrorContext); // Array of error objects
+
+  const isSpectator = () =>
+    !userToken || (user && user.lobby.game.id !== parseInt(gameId));
+
+  const fetchGame = async () => {
+    try {
+      const response = await axios.get(`/games/${gameId}`);
+      setGame(response.data);
+    } catch (error) {
+      setErrors([...errors, error.response.data]);
+    }
+  };
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get(`/users/${loggedUser.username}`);
+      setUser(response.data);
+    } catch (error) {
+      setErrors([...errors, error.response.data]);
+    }
+  };
+
+  useEffect(() => {
+    // fetch game status on page load
+    fetchGame();
+    if (!isSpectator()) fetchUser();
+  }, []);
+
+  return (
+    <>
+      {isSpectator() && <SpectatorToast />}
+      <h1>This is game {gameId}</h1>;
+    </>
+  );
 }
