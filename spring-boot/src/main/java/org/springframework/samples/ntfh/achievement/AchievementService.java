227d1d636cbf9f9da85b62c642d7cfbef96f6997
andrsdt <andresdt@pm.me>
2021-11-25 15:55:38 +0100

    fix: checking right the opposite

--- spring-boot/src/main/java/org/springframework/samples/ntfh/achievement/AchievementService.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/achievement/AchievementService.java
@@ -34,7 +34,7 @@ public class AchievementService {
     @Transactional
     public Achievement updateAchievement(Achievement achievement, String token) throws DataAccessException,
             DataIntegrityViolationException, NonMatchingTokenException, IllegalArgumentException {
-        
+
         if (achievement.getName().isEmpty()) {
             throw new IllegalArgumentException("The name cannot be empty.");
         }
@@ -43,7 +43,7 @@ public class AchievementService {
             throw new IllegalArgumentException("The description cannot be empty.");
         }
 
-        if(TokenUtils.tokenHasAnyAuthorities(token, "admin")) {
+        if (!TokenUtils.tokenHasAnyAuthorities(token, "admin")) {
             throw new NonMatchingTokenException("Only admins can edit achievements.");
         }
 
