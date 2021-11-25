a34af8d837011dc653c63429cf5ca676a290ae14
andrsdt <andresdt@pm.me>
2021-11-25 23:53:02 +0100

    refactor: extract GamesHistoryTable to compnent

--- react/src/pages/admin-page.js
+++ react/src/pages/admin-page.js
@@ -1,21 +1,41 @@
 import { useContext, useEffect, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { useHistory } from "react-router";
-import Homebar from "../components/home/Homebar";
-import UserContext from "../context/user";
-import hasAuthority from "../helpers/hasAuthority";
-import tokenParser from "../helpers/tokenParser";
-import * as ROUTES from "../constants/routes";
+import axios from "../api/axiosConfig";
 import AchievementsTable from "../components/admin/AchievementsTable";
 import GamesHistoryTable from "../components/admin/GamesHistoryTable";
 import OngoingGamesTable from "../components/admin/OngoingGamesTable";
 import UsersTable from "../components/admin/UsersTable";
+import Homebar from "../components/home/Homebar";
+import * as ROUTES from "../constants/routes";
+import errorContext from "../context/error";
+import userContext from "../context/user";
+import hasAuthority from "../helpers/hasAuthority";
+import tokenParser from "../helpers/tokenParser";
 
 export default function AdminPage() {
   const history = useHistory();
-  const user = tokenParser(useContext(UserContext));
-
+  const user = tokenParser(useContext(userContext));
+  const { userToken } = useContext(userContext);
   const [currentTable, setCurrentTable] = useState("games");
+  const [gamesHistory, setGamesHistory] = useState([]);
+  const { errors, setErrors } = useContext(errorContext);
+
+  useEffect(() => {
+    const fetchGameHistory = async () => {
+      if (currentTable !== "gamesHistory") return;
+      try {
+        // TODO remove auth if not needed
+        const headers = { Authorization: "Bearer " + userToken };
+        const response = await axios.get(`gameHistory`, { headers });
+
+        gamesHistory(response.data);
+      } catch (error) {
+        setErrors([...errors, error.response]);
+      }
+    };
+    fetchGameHistory();
+  }, [currentTable]);
 
   useEffect(() => {
     document.title = "NTFH - Admin panel";
@@ -61,7 +81,9 @@ export default function AdminPage() {
         </Row>
         <Row>
           {currentTable === "ongoingGames" && <OngoingGamesTable />}
-          {currentTable === "gamesHistory" && <GamesHistoryTable />}
+          {currentTable === "gamesHistory" && (
+            <GamesHistoryTable data={gamesHistory} />
+          )}
           {currentTable === "users" && <UsersTable />}
           {currentTable === "achievements" && <AchievementsTable />}
         </Row>
