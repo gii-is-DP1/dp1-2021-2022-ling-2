33521b7a503e29de2d7d102916761d3bf463e00b
Icaro212 <pablorcmail@gmail.com>
2021-11-25 01:43:37 +0100

    H3 TEST

--- spring-boot/src/test/java/org/springframework/samples/ntfh/marketcard/MarketCardServiceTest.java
+++ spring-boot/src/test/java/org/springframework/samples/ntfh/marketcard/MarketCardServiceTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.assertj.core.util.Lists;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
--- spring-boot/src/test/java/org/springframework/samples/ntfh/user/UserServiceTest.java
+++ spring-boot/src/test/java/org/springframework/samples/ntfh/user/UserServiceTest.java
@@ -1,5 +1,47 @@
 package org.springframework.samples.ntfh.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class UserServiceTest {
     // TODO implement when JWT authentication is ready
+
+    @Autowired
+    private UserService userService;
+    // @Autowired
+    // private UserController userController;
+
+    @Test
+    public void testPH3E1(){
+        // User user= new User();
+        // user.setUsername("andres");
+        // user.setPassword("andres");
+        // userController.login(user);
+        List<User> l = new ArrayList<>();
+        userService.findAll().forEach(x->l.add(x));
+        assertEquals(2, l.size());
+    }
+
+    // public Void voidNH3E1(){
+    //     User user= new User();
+    //     user.setUsername("andres");
+    //     user.setPassword("andres");
+    //     userController.login(user);
+    //     List<User> l = new ArrayList<>();
+    //     userService.findAll().forEach(l.add(x)->);
+    // }
+
+
+
 }
