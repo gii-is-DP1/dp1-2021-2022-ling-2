648a5011b5dc25a8e89c4e96cc49eb4216c9b79a
andrsdt <andresdt@pm.me>
2021-11-26 01:22:01 +0100

    refactor: remove unused imports

--- spring-boot/src/main/java/org/springframework/samples/ntfh/game/history/GameHistoryController.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/game/history/GameHistoryController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/gameHistory")
 public class GameHistoryController {
-    
+
     @Autowired
     private GameHistoryService gameHistoryService;
     @Autowired
-    private GameHistoryRepository gameHistoryRepository; 
+    private GameHistoryRepository gameHistoryRepository;
 
     @GetMapping
     public ResponseEntity<Iterable<GameHistory>> getAll() {
