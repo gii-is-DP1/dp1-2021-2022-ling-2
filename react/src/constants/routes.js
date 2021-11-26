dddca5320bac09930755a349fbd65ca2a13bcd47
andrsdt <andresdt@pm.me>
2021-11-26 01:23:42 +0100

    add: all achievements page

--- react/src/constants/routes.js
+++ react/src/constants/routes.js
@@ -11,3 +11,4 @@ export const LOBBY = "/lobbies/:lobbyId";
 export const NOT_FOUND = "/not-found";
 export const ADMIN_PAGE = "/admin";
 export const STATISTICS = "/statistics";
+export const ACHIEVEMENTS = "/achievements";
---
+++ react/src/pages/achievements.js
@@ -0,0 +1,10 @@
+import AchievementsTable from "../components/admin/AchievementsTable";
+
+export default function Achievements() {
+  return (
+    <>
+      <h1>Achievements</h1>
+      <AchievementsTable />
+    </>
+  );
+}
