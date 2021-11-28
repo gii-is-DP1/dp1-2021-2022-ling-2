7304d16d927ec7c501450592f28d540fd69c026f
andrsdt <andresdt@pm.me>
2021-11-28 12:23:19 +0100

    add: achievements table in tailwindcss

--- react/src/components/admin/AchievementsTable.js
+++ react/src/components/admin/AchievementsTable.js
@@ -27,33 +27,57 @@ export default function AchievementsTable() {
   }, []);
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        {achievements.map((achievement, idx) => (
-          <tr key={idx}>
-            <th>{achievement.name}</th>
-            <th>{achievement.description}</th>
-            <th>
-              <Link
-                to={ROUTES.EDIT_ACHIEVEMENT.replace(
-                  ":achievementId",
-                  achievement.id
-                )}
-              >
-                {hasAuthority(user, "admin") && (
-                  <Button type="submit">Edit</Button>
-                )}
-              </Link>
-            </th>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-10 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Achievements
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {achievements.map((achievement, idx) => (
+                  <tr key={idx}>
+                    <td className="px-6 py-4 ">
+                      <div className="flex items-center">
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {achievement.name}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {achievement.description}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <Link
+                        className="text-indigo-600 hover:text-indigo-900"
+                        to={ROUTES.EDIT_ACHIEVEMENT.replace(
+                          ":achievementId",
+                          achievement.id
+                        )}
+                      >
+                        Edit
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
