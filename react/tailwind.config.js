f578dc3eccc6afc9cdfc8910547e66613c274c8a
andrsdt <andresdt@pm.me>
2021-11-28 12:21:57 +0100

    add: custom backgrounds

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // TODO this should be in /public/ folder
+        wood: "url('images/backgrounds/wood.png')",
+        green: "url('images/backgrounds/green.png')",
+      },
+    },
   },
   variants: {
     extend: {},
