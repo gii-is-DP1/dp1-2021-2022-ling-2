665e71e90cd15b1d07690bdca85002ab777ffdb6
andrsdt <andresdt@pm.me>
2021-12-01 17:56:29 +0100

    add: edit profile in tailwindcss

--- react/src/pages/edit-profile.js
+++ react/src/pages/edit-profile.js
@@ -1,10 +1,9 @@
 import { useContext, useEffect, useState } from "react";
-import { Button, Form } from "react-bootstrap";
 import toast from "react-hot-toast";
 import { useHistory, useParams } from "react-router-dom";
 import axios from "../api/axiosConfig";
-import Homebar from "../components/home/Homebar";
 import * as ROUTES from "../constants/routes";
+import user from "../context/user";
 import userContext from "../context/user";
 import tokenParser from "../helpers/tokenParser";
 
@@ -51,6 +50,7 @@ export default function EditProfile() {
         username,
         email,
       };
+      // Only send the pasword if it has been changed
       if (password !== "") payload.password = password;
 
       const response = await axios.put("/users", payload, {
@@ -62,7 +62,7 @@ export default function EditProfile() {
       toast.success("Profile edited successfully");
       sendToProfile();
     } catch (error) {
-      toast.error(error.response?.data);
+      toast.error(error.response?.data?.message);
     }
   }
 
@@ -85,54 +85,57 @@ export default function EditProfile() {
   }, []); // Empty array means "run only first time the component renders"
 
   return (
-    <>
-      <Homebar />
-      <h1>Edit your profile</h1>
-      <br />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            name="username"
-            defaultValue={userProfile?.username}
+    <div className="flex flex-col h-screen bg-wood p-8 items-center">
+      <span className="text-center pb-8">
+        <button type="submit" className="btn-ntfh">
+          <p className="text-5xl text-gradient-ntfh">Edit profile</p>
+        </button>
+      </span>
+      <form className="flex flex-col bg-felt rounded-3xl border-20 border-gray-900 p-8">
+        <div className="flex flex-col mb-4">
+          <p className="font-bold text-2xl mb-2">Username</p>
+          <input
             disabled
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
+            value={params.username}
+            type="text"
+            className="p-3 rounded-xl border-4 border-black text-black"
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+        </div>
+        <div className="flex flex-col mb-4">
+          <p className="font-bold text-2xl mb-2">Email</p>
+          <input
+            defaultValue={email}
+            placeholder="user@mail.com"
             type="email"
-            placeholder="Enter email"
-            name="email"
-            defaultValue={userProfile?.email}
+            className="p-3 rounded-xl border-4 border-black text-black"
             onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Confirm password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm password"
-            name="confirmPassword"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </>
+          ></input>
+        </div>
+        <div className="flex flex-row justify-between space-x-4">
+          <div className="flex flex-col mb-4">
+            <p className="font-bold text-2xl mb-2">Password</p>
+            <input
+              placeholder="********"
+              type="password"
+              className="p-3 rounded-xl border-4 border-black text-black"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          <div className="flex flex-col mb-8">
+            <p className="font-bold text-2xl mb-2">Confirm Password</p>
+            <input
+              placeholder="********"
+              type="password"
+              className="p-3 rounded-xl border-4 border-black text-black"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <button type="submit" className="btn-ntfh" onClick={handleSubmit}>
+          <p className="text-gradient-ntfh text-5xl p-2">Edit</p>
+        </button>
+      </form>
+    </div>
   );
 }
