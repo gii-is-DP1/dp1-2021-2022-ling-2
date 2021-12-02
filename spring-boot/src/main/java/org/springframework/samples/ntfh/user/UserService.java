ed741aaac60f2744b53c727e807791877a91b9ea
andrsdt <andresdt@pm.me>
2021-12-02 21:55:15 +0100

    add: non-paginated findAll for service

--- spring-boot/src/main/java/org/springframework/samples/ntfh/user/UserService.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/user/UserService.java
@@ -102,6 +102,11 @@ public class UserService {
 	}
 
 	@Transactional(readOnly = true)
+	public Iterable<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	@Transactional(readOnly = true)
 	public Page<User> findAllPage(Pageable pageable) {
 		return userRepository.findAllPage(pageable);
 	}
