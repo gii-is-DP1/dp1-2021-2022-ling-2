c2340ae5b0d58cfd0c9a488f34f0971c6d16ae9d
andrsdt <andresdt@pm.me>
2021-12-02 16:09:22 +0100

    fix: center felt box

--- react/src/pages/create-lobby.js
+++ react/src/pages/create-lobby.js
@@ -52,73 +52,75 @@ export default function CreateLobby() {
       <div className="flex-none btn-ntfh text-5xl">
         <p className="text-gradient-ntfh">Create game</p>
       </div>
-      <div className="bg-felt w-2/3 lg:w-1/2 2xl:w-1/3 my-16 rounded-3xl border-20 border-gray-900 p-8">
-        <div className="flex flex-col items-center space-y-8">
-          <div className="flex flex-col w-full">
-            {/* game name and text input field */}
-            <p className="font-bold text-3xl text-center mb-2">Game name</p>
-            <input
-              className="p-3 rounded-xl border-4 border-black"
-              defaultValue={gameName}
-              placeholder={gameName}
-              onChange={(e) => setGameName(e.target.value)}
-            ></input>
-          </div>
-          <span className="flex flex-row items-baseline space-x-4">
-            {/* number of players and selector of 3 */}
-            <p className={"font-bold text-xl"}>Max players</p>
-            <button
-              className={`btn-radio-ntfh ${
-                maxPlayers === 2 && "bg-green-600 text-white"
-              }`}
-              onClick={() => setMaxPlayers(2)}
-            >
-              2
-            </button>
+      <div className="flex-1 flex flex-col w-2/3 lg:w-1/2 2xl:w-1/3 justify-center">
+        <div className="bg-felt rounded-3xl border-20 border-gray-900 p-8">
+          <div className="flex flex-col items-center space-y-8">
+            <div className="flex flex-col w-full">
+              {/* game name and text input field */}
+              <p className="font-bold text-3xl text-center mb-2">Game name</p>
+              <input
+                className="p-3 rounded-xl border-4 border-black"
+                defaultValue={gameName}
+                placeholder={gameName}
+                onChange={(e) => setGameName(e.target.value)}
+              ></input>
+            </div>
+            <span className="flex flex-row items-baseline space-x-4">
+              {/* number of players and selector of 3 */}
+              <p className={"font-bold text-xl"}>Max players</p>
+              <button
+                className={`btn-radio-ntfh ${
+                  maxPlayers === 2 && "bg-green-600 text-white"
+                }`}
+                onClick={() => setMaxPlayers(2)}
+              >
+                2
+              </button>
 
-            <button
-              className={`btn-radio-ntfh ${
-                maxPlayers === 3 && "bg-green-600 text-white"
-              }`}
-              onClick={() => setMaxPlayers(3)}
-            >
-              3
+              <button
+                className={`btn-radio-ntfh ${
+                  maxPlayers === 3 && "bg-green-600 text-white"
+                }`}
+                onClick={() => setMaxPlayers(3)}
+              >
+                3
+              </button>
+              <button
+                className={`btn-radio-ntfh ${
+                  maxPlayers === 4 && "bg-green-600 text-white"
+                }`}
+                onClick={() => setMaxPlayers(4)}
+              >
+                4
+              </button>
+            </span>
+            <span className="flex flex-row items-baseline space-x-4">
+              {/* scenes and checkbox */}
+              <p className="font-bold text-xl">Scenes</p>
+              <button
+                id="checkScenes"
+                className={"btn-checkbox-ntfh"}
+                onClick={(e) => setScenesChecked(!scenesChecked)}
+              >
+                {scenesChecked ? "✔️" : "❌"}
+              </button>
+            </span>
+            <span className="flex flex-row items-baseline space-x-4">
+              {/* spectators and checkbox */}
+              <p className="font-bold text-xl">Spectators</p>
+              <button
+                id="checkSpectators"
+                className="btn-checkbox-ntfh"
+                onClick={(e) => setSpectatorsChecked(!spectatorsChecked)}
+              >
+                {spectatorsChecked ? "✔️" : "❌"}
+              </button>
+            </span>
+            {/* start game button */}
+            <button className="btn-ntfh" onClick={handleCreateLobby}>
+              <p className="text-gradient-ntfh">Create lobby</p>
             </button>
-            <button
-              className={`btn-radio-ntfh ${
-                maxPlayers === 4 && "bg-green-600 text-white"
-              }`}
-              onClick={() => setMaxPlayers(4)}
-            >
-              4
-            </button>
-          </span>
-          <span className="flex flex-row items-baseline space-x-4">
-            {/* scenes and checkbox */}
-            <p className="font-bold text-xl">Scenes</p>
-            <button
-              id="checkScenes"
-              className={"btn-checkbox-ntfh"}
-              onClick={(e) => setScenesChecked(!scenesChecked)}
-            >
-              {scenesChecked ? "✔️" : "❌"}
-            </button>
-          </span>
-          <span className="flex flex-row items-baseline space-x-4">
-            {/* spectators and checkbox */}
-            <p className="font-bold text-xl">Spectators</p>
-            <button
-              id="checkSpectators"
-              className="btn-checkbox-ntfh"
-              onClick={(e) => setSpectatorsChecked(!spectatorsChecked)}
-            >
-              {spectatorsChecked ? "✔️" : "❌"}
-            </button>
-          </span>
-          {/* start game button */}
-          <button className="btn-ntfh" onClick={handleCreateLobby}>
-            <p className="text-gradient-ntfh">Create lobby</p>
-          </button>
+          </div>
         </div>
       </div>
     </div>
