da13c195d89e83e1c4e06d2ee69a6af4f5cd1c6e
andrsdt <andresdt@pm.me>
2021-12-04 20:45:33 +0100

    add: idea about STATE pattern

--- spring-boot/src/main/java/org/springframework/samples/ntfh/enemy/ingame/EnemyLocation.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/enemy/ingame/EnemyLocation.java
@@ -1,11 +1,12 @@
 package org.springframework.samples.ntfh.enemy.ingame;
 
-import org.springframework.samples.ntfh.interfaces.Location;
-
 /**
  * Applicable for both HordeEnemyIngame and WardlordIngame entities in principle
  */
 public enum EnemyLocation {
+    // TODO this could be a good chance to apply the state design pattern, since the
+    // functionality of the card is different depending on its location (state)
+
     /** The enemy is still waiting to fight */
     PILE,
 
