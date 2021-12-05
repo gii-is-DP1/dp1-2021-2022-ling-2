91526a22df7d59ab5332d2869891b60d19111e80
andrsdt <andresdt@pm.me>
2021-12-05 20:01:38 +0100

    add: entity validation errors handling

--- spring-boot/src/main/java/org/springframework/samples/ntfh/configuration/ExceptionHandlerConfiguration.java
+++ spring-boot/src/main/java/org/springframework/samples/ntfh/configuration/ExceptionHandlerConfiguration.java
@@ -3,11 +3,14 @@ package org.springframework.samples.ntfh.configuration;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.ntfh.exceptions.BannedUserException;
@@ -15,8 +18,10 @@ import org.springframework.samples.ntfh.exceptions.MaximumLobbyCapacityException
 import org.springframework.samples.ntfh.exceptions.MissingAttributeException;
 import org.springframework.samples.ntfh.exceptions.NonMatchingTokenException;
 import org.springframework.samples.ntfh.exceptions.UserAlreadyInLobbyException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -82,6 +87,22 @@ public class ExceptionHandlerConfiguration extends ResponseEntityExceptionHandle
     @ExceptionHandler(MissingAttributeException.class)
     public ResponseEntity<Object> missingAttributeExceptionHandler(HttpServletRequest request,
             MaximumLobbyCapacityException ex) {
+        return buildResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // Validator for annotation restrictions in entities
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> constraintViolationExceptionHandler(HttpServletRequest request,
+            ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations().stream().map(ConstraintViolation::getMessage)
+                .reduce((s1, s2) -> s1 + " " + s2).orElse("");
+        return buildResponseEntity(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
         return buildResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
