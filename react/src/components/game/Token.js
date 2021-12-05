7957a056e56c2056a7e8160dd493df7f63d63414
andrsdt <andresdt@pm.me>
2021-12-05 12:08:19 +0100

    add: better looking kill, glory, gold tokens

--- react/src/components/game/Token.js
+++ react/src/components/game/Token.js
@@ -1,16 +1,10 @@
-import { DEFAULT_IMAGE_PATH } from "../../constants/paths";
-
 export default function Token(params) {
   const { type, value } = params;
   return (
-    //   TODO replace <img> with <div>
     <>
-      <img
-        src={`${DEFAULT_IMAGE_PATH}/tokens/${type}.png`}
-        alt={`${type} token`}
-        className={`w-10 token token-${type}`}
-      />
-      {/* <p>{value}</p> */}
+      <div className={`flex items-center justify-center token token-${type}`}>
+        <p>{value}</p>
+      </div>
     </>
   );
 }
--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
       backgroundImage: {
         // TODO this should be in /public/ folder
         wood: "url('images/backgrounds/wood.png')",
-        green: "url('images/backgrounds/green.png')",
         felt: "url('images/backgrounds/felt.png')",
         fabric: "url('images/backgrounds/fabric.png')",
         leather: "url('images/backgrounds/leather.png')",
