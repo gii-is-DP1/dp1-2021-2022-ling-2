09814d2772ba94be3ada75cf05140728645364d9
andrsdt <andresdt@pm.me>
2021-12-06 18:56:56 +0100

    add: image routes

--- react/src/constants/images.js
+++ react/src/constants/images.js
@@ -1,3 +1,5 @@
 import { BASE_IMAGE_PATH } from "./paths";
 export const LOGO = `${BASE_IMAGE_PATH}/logo_ntfh.png`;
 export const CARD_BACK = `${BASE_IMAGE_PATH}/cards/back_standard.png`;
+export const WARLORD_CARD_BACK = `${BASE_IMAGE_PATH}/cards/enemies/warlords/back_standard.png`;
+export const SCENE_CARD_BACK = `${BASE_IMAGE_PATH}/cards/scenes/back_standard.png`;
--- react/src/constants/paths.js
+++ react/src/constants/paths.js
@@ -1,2 +1,3 @@
-export const BASE_IMAGE_PATH = "/images";
 export const API_BASE_URL = "http://localhost:8080/api";
+export const BASE_IMAGE_PATH = "/images";
+export const CHARACTER_IMAGE_PATH = BASE_IMAGE_PATH + "/cards/characters";
