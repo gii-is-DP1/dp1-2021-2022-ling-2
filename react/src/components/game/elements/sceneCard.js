801ffe8627fb0f4ed42b998ad5e324678d06f7fb
andrsdt <andresdt@pm.me>
2021-12-06 23:28:16 +0100

    add: zoomable scene

--- react/src/components/game/elements/sceneCard.js
+++ react/src/components/game/elements/sceneCard.js
@@ -8,13 +8,13 @@ export default function SceneCard(params) {
     <>
       {scene ? (
         <img
-          className="card hover:scale-400"
+          className="card zoomable hover:scale-400"
           src={`${SCENE_IMAGE_PATH}/${scene.sceneTypeEnum.toLowerCase()}.png`}
           alt={scene.sceneTypeEnum}
         ></img>
       ) : (
         <div
-          className="card flex items-center justify-center h-full bg-contain bg-no-repeat hover:scale-250"
+          className="card zoomable flex items-center justify-center h-full bg-contain bg-no-repeat hover:scale-200"
           style={{
             backgroundImage: `url('${SCENE_CARD_BACK}')`,
           }}
