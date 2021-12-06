655fe5e32b764aaacfbf01a6b0cdd12a2994a3f6
andrsdt <andresdt@pm.me>
2021-12-06 22:29:07 +0100

    add: Turn entity

--- spring-boot/src/main/java/org/springframework/ntfh/entity/turn/Turn.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/turn/Turn.java
@@ -5,8 +5,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.springframework.ntfh.entity.game.Game;
 import org.springframework.ntfh.entity.model.BaseEntity;
+import org.springframework.ntfh.entity.player.Player;
 import org.springframework.ntfh.entity.scene.Scene;
 
 import lombok.Getter;
@@ -19,7 +23,13 @@ public class Turn extends BaseEntity {
 
     @OneToOne(optional = false)
     @JoinColumn(name = "game_id")
+    @JsonIgnoreProperties({ "startTime" })
     private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id")
+    @JsonIgnoreProperties({ "glory", "kills", "gold", "wounds", "user", "characterType" })
+    private Player player;
 
     @ManyToOne(optional = true)
     @JoinColumn(name = "scene_id")
--- spring-boot/src/main/java/org/springframework/ntfh/entity/turn/TurnService.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/turn/TurnService.java
@@ -1,10 +1,14 @@
 package org.springframework.ntfh.entity.turn;
 
 import java.util.Optional;
+import java.util.Random;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ntfh.entity.game.Game;
+import org.springframework.ntfh.entity.scene.Scene;
+import org.springframework.ntfh.entity.scene.SceneService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,9 @@ public class TurnService {
 
     @Autowired
     private TurnRepository turnRepository;
+
+    @Autowired
+    private SceneService sceneService;
 
     @Transactional
     public Integer turnCount() {
@@ -36,4 +43,29 @@ public class TurnService {
     public void delete(int turnId) {
         turnRepository.deleteById(turnId);
     }
+
+    /**
+     * Create a turn given a game
+     * 
+     * @author andrsdt
+     * @param game that the turn will be created for
+     */
+    @Transactional
+    public void createFromGame(Game game) {
+        Turn turn = new Turn();
+        turn.setPlayer(game.getLeader()); // TODO create a Integer turnOrder in Player, a transient getter
+
+        if (game.getHasScenes()) {
+            // Get a random scene and set it as the current scene
+            Scene randomScene = sceneService.findSceneById(new Random().nextInt(sceneService.count())).get();
+            turn.setCurrentScene(randomScene);
+        }
+
+        turn.setGame(game);
+        turnRepository.save(turn);
+
+        // Set a foreign key to the current turn in the game
+        game.setCurrentTurn(turn);
+    }
+
 }
