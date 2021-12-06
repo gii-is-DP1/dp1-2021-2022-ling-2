11f78e3beab1fbce358321d0560ab29a13f95ac2
andrsdt <andresdt@pm.me>
2021-12-06 19:18:57 +0100

    add: dynamic zoom adjusted for viewport

--- react/src/components/game/elements/characterCard.js
+++ react/src/components/game/elements/characterCard.js
@@ -5,7 +5,9 @@ export default function CharacterCard(params) {
 
   return (
     <img
-      className="card hover:scale-500 hover:-translate-y-40"
+      className="card hover:scale-500 2xl:hover:scale-350 hover:-translate-y-40
+      2xl:hover:-translate-y-50
+      "
       src={`${CHARACTER_IMAGE_PATH}/${character.characterTypeEnum.toLowerCase()}_${character.characterGenderEnum.toLowerCase()}.png`}
       alt={`${character.characterTypeEnum} ${character.characterGenderEnum}`}
     ></img>
