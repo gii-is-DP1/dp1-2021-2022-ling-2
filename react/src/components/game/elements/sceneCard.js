15b2182c33d5f3352885758e408a934d9163a5b5
andrsdt <andresdt@pm.me>
2021-12-06 20:13:03 +0100

    add: sceneCard component

---
+++ react/src/components/game/elements/sceneCard.js
@@ -0,0 +1,30 @@
+import { SCENE_CARD_BACK } from "../../../constants/images";
+import { SCENE_IMAGE_PATH } from "../../../constants/paths";
+import Token from "./token";
+
+export default function SceneCard(params) {
+  const { scene, count } = params;
+  return (
+    <>
+      {scene ? (
+        <img
+          className="card hover:scale-250 hover:translate-y-20"
+          src={`${SCENE_IMAGE_PATH}/${scene.sceneTypeEnum.toLowerCase()}.png`}
+          alt={scene.sceneTypeEnum}
+        ></img>
+      ) : (
+        <div
+          className="card flex items-center justify-center h-full bg-contain bg-no-repeat hover:scale-250"
+          style={{
+            backgroundImage: `url('${SCENE_CARD_BACK}')`,
+          }}
+          alt={`Market card pile (${count} items)`}
+        >
+          <div className="transform-gpu -rotate-90">
+            <Token type="count" value={count} />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
