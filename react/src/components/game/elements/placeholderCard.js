69a19ea17cb431b3810d70aaaf5c05543348c3a5
andrsdt <andresdt@pm.me>
2021-12-06 18:58:45 +0100

    add: minor animation improvements

--- react/src/components/game/elements/placeholderCard.js
+++ react/src/components/game/elements/placeholderCard.js
@@ -3,7 +3,7 @@ import { CARD_BACK } from "../../../constants/images";
 export default function PlaceholderCard(params) {
   return (
     <img
-      className="card hover:scale-250 hover:-translate-y-32"
+      className="card hover:scale-250 hover:-translate-y-20"
       src={CARD_BACK}
       // template CARD_BACK
       alt="PLACEHOLDER CARD"
