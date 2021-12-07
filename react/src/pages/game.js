d70f35c5dbb07b4138951533c180888ab75f0a46
andrsdt <andresdt@pm.me>
2021-12-07 10:21:55 +0100

    add: show current user on bottom left, others in turn order

--- react/src/pages/game.js
+++ react/src/pages/game.js
@@ -26,12 +26,31 @@ export default function Game() {
   const isSpectator = () =>
     !userToken || (user && user?.lobby?.game?.id !== parseInt(gameId));
 
+  const playersInRenderOrder = (_players) => {
+    const orderedPlayerList = _players.sort(
+      (a, b) => a.turnOrder - b.turnOrder
+    );
+    /* In case we are someone playing the game and not a spectator, make sure
+     * that the first player of the list (the one who will be rendered on the
+     * bottom left part) is us
+     * */
+    if (!isSpectator()) {
+      // Rotate the list until the current player is at the first position,
+      // but they still keep the same order (by rotating values in array)
+      while (orderedPlayerList[0].user.username !== loggedUser.username) {
+        orderedPlayerList.push(orderedPlayerList.shift());
+      }
+    }
+    return orderedPlayerList;
+  };
+
   const fetchGame = async () => {
     try {
       const response = await axios.get(`/games/${gameId}`);
       const _game = response.data;
       setGame(_game);
-      setPlayers(_game.players);
+      const sortedPlayers = playersInRenderOrder(_game.players);
+      setPlayers(sortedPlayers);
     } catch (error) {
       toast.error(error.response?.data?.message);
       if (error.response?.status === 404) history.push(ROUTES.HOME);
@@ -69,15 +88,15 @@ export default function Game() {
       <div className="bg-wood h-screen px-16 flex flex-col justify-center">
         {/* Top player names */}
         <div className="flex-none flex justify-between items-center p-2 text-white text-3xl">
-          <p>{players[2] && players[2].user.username}</p>
           <p>{players[3] && players[3].user.username}</p>
+          <p>{players[2] && players[2].user.username}</p>
         </div>
         {/* Game board (felt part)*/}
         <div className="flex-1 bg-felt rounded-3xl">
           <div className="h-full p-2 grid grid-cols-5 gap-4">
             <div className="row-span-2">
               {players[2] && (
-                <PlayerZoneVertical player={players[2]} rotation={90} />
+                <PlayerZoneVertical player={players[3]} rotation={90} />
               )}
               {/* Top left */}
             </div>
@@ -87,23 +106,23 @@ export default function Game() {
             <div className="row-span-2">
               {players[3] && (
                 <PlayerZoneVertical
-                  player={players[3]}
+                  player={players[2]}
                   rotation={90}
                   counterclockwise
                 />
               )}
-              {/* top right */}
+              {/* Top right */}
             </div>
             <div className="col-span-2 self-end max-w-xs 2xl:max-w-sm">
               {players[0] && <PlayerZoneHorizontal player={players[0]} />}
-              {/* bottom left */}
+              {/* Bottom left (My hand) */}
             </div>
             <div className="self-end">{/* Blank space */}</div>
             <div className="col-span-2 self-end justify-self-end max-w-xs 2xl:max-w-sm">
               {players[1] && (
                 <PlayerZoneHorizontal player={players[1]} reverse />
               )}
-              {/* bottom right */}
+              {/* Bottom right */}
             </div>
           </div>
         </div>
