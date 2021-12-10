d5ebb047235f95806a327afef78d7c113ad2a815
andrsdt <andresdt@pm.me>
2021-12-10 17:25:27 +0100

    add: missing interface attributes

--- react/src/interfaces/User.ts
+++ react/src/interfaces/User.ts
@@ -1,11 +1,13 @@
 import { Authority } from "./Authority";
 import { Character } from "./Character";
+import { Lobby } from "./Lobby";
 
 export interface User {
   username: string;
   password: string;
   email: string;
   authorities: Authority[];
+  lobby: Lobby;
   character: Character;
   enabled: boolean;
 }
