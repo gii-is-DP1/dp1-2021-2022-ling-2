9f30e751da816500b220036a49eabf5336a5b50b
andrsdt <andresdt@pm.me>
2021-12-18 20:36:56 +0100

    add: abilityCard and its ingame entities

---
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/playablecard/abilitycard/ingame/AbilityCardIngameRepository.java
@@ -0,0 +1,6 @@
+package org.springframework.ntfh.entity.playablecard.abilitycard.ingame;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AbilityCardIngameRepository extends CrudRepository<AbilityCardIngame, Integer> {
+}
---
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/playablecard/abilitycard/ingame/AbilityCardLocationEnum.java
@@ -0,0 +1,5 @@
+package org.springframework.ntfh.entity.playablecard.abilitycard.ingame;
+
+public enum AbilityCardLocationEnum {
+    HAND, DISCARD_PILE, ABILITY_PILE
+}
