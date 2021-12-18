b2dbcd684461639247a781006912d92802c04b57
andrsdt <andresdt@pm.me>
2021-12-18 20:34:29 +0100

    refactor: remove unused classes

--- spring-boot/src/main/java/org/springframework/ntfh/entity/playablecard/charactercard/ingame/CharacterCardIngame.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.ntfh.entity.playablecard.charactercard.ingame;
-
-public class CharacterCardIngame {
-    // Same dilemma: will this hold all the character cards, no matter their type?
-    // Or should there be a separate table for each type?
-}
