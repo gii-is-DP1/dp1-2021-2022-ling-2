188597bdaca1eac0c0d4809d81f8d9d0fdb76b22
andrsdt <andresdt@pm.me>
2021-12-18 20:42:15 +0100

    add: stage in turn entity

--- spring-boot/src/main/java/org/springframework/ntfh/entity/turn/Turn.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/turn/Turn.java
@@ -1,11 +1,13 @@
 package org.springframework.ntfh.entity.turn;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.springframework.ntfh.entity.game.Game;
@@ -21,17 +23,22 @@ import lombok.Setter;
 @Entity
 public class Turn extends BaseEntity {
 
+    // TODO is this bidirectionality needed? I don't think so
     @OneToOne(optional = false)
-    @JoinColumn(name = "game_id")
+    @JoinColumn(name = "game_id") // TODO needed?
     @JsonIgnoreProperties({ "startTime" })
     private Game game;
 
     @ManyToOne
-    @JoinColumn(name = "player_id")
+    @JoinColumn(name = "player_id") // TODO needed?
     @JsonIgnoreProperties({ "glory", "kills", "gold", "wounds", "user", "characterType" })
     private Player player;
 
     @ManyToOne(optional = true)
-    @JoinColumn(name = "scene_id")
+    @JoinColumn(name = "scene_id") // TODO needed?
     private Scene currentScene;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private TurnStageEnum stage;
 }
