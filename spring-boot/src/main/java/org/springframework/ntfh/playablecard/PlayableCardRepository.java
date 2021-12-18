79e84db114c8fe72114d39da3bf4e64fe1f5ef33
andrsdt <andresdt@pm.me>
2021-12-18 20:35:52 +0100

    refactor: remove unused PlayableCard MVC

--- spring-boot/src/main/java/org/springframework/ntfh/playablecard/PlayableCardRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.ntfh.playablecard;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayableCardRepository extends CrudRepository<PlayableCard, Integer> {
-
-}
