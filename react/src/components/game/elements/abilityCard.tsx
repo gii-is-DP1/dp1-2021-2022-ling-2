e43983eb64b615474be3dd8f22f4743319bd772f
andrsdt <andresdt@pm.me>
2021-12-25 04:54:39 +0100

    refactor: extract variable

--- react/src/components/game/elements/abilityCard.tsx
+++ react/src/components/game/elements/abilityCard.tsx
@@ -3,17 +3,20 @@ import { AbilityCardIngame } from "../../../interfaces/AbilityCardIngame";
 type Params = {
   card: AbilityCardIngame;
 };
+
 export default function AbilityCard(params: Params) {
   const { card } = params;
+
+  const abilityCardTypeEnum = card.abilityCard.abilityCardTypeEnum;
+
   return (
     <img
       className={`card transform-gpu
       border-2 border-yellow-300 border-opacity-0 hover:border-opacity-100
       hover:-translate-y-6 
       `}
-      src={`/images/cards/characters/${card.abilityCard.characterTypeEnum.toLowerCase()}/${card.abilityCard.abilityCardTypeEnum.toLowerCase()}.png`}
-      // template CARD_BACK
-      alt={card.abilityCard.abilityCardTypeEnum.toLowerCase().toString()}
+      src={`/images/cards/characters/${card.abilityCard.characterTypeEnum.toLowerCase()}/${abilityCardTypeEnum.toLowerCase()}.png`}
+      alt={abilityCardTypeEnum.toLowerCase().toString()}
     ></img>
   );
 }
