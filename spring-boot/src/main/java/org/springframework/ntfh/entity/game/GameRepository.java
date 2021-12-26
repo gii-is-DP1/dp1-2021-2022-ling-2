a798eec5a2c064d298bc827a2d13a97f6dcab7ac
andrsdt <andresdt@pm.me>
2021-12-26 02:34:02 +0100

    add: getCurrenTurnByGameId

--- spring-boot/src/main/java/org/springframework/ntfh/entity/game/GameRepository.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/game/GameRepository.java
@@ -1,7 +1,10 @@
 package org.springframework.ntfh.entity.game;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.ntfh.entity.turn.Turn;
 
 public interface GameRepository extends CrudRepository<Game, Integer> {
-
+    @Query("SELECT g.currentTurn FROM Game g WHERE g.id = ?1")
+    Turn getCurrentTurnByGameId(Integer gameId);
 }
