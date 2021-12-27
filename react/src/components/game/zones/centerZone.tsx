5bfa6d0957d1edc748d47abaedbac1a9f0fda7d2
andrsdt <andresdt@pm.me>
2021-12-27 17:30:12 +0100

    add: marketCard piles separation in Game

--- react/src/components/game/zones/centerZone.tsx
+++ react/src/components/game/zones/centerZone.tsx
@@ -1,51 +1,34 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "../../../api/axiosConfig";
+import gameContext from "../../../context/game";
 import { AbilityCardIngame } from "../../../interfaces/AbilityCardIngame";
-import { Game } from "../../../interfaces/Game";
-import { HordeEnemyIngame } from "../../../interfaces/HordeEnemyIngame";
-import { MarketCardIngame } from "../../../interfaces/MarketCardIngame";
-import { WarlordIngame } from "../../../interfaces/WarlordIngame";
 import CountCard from "../elements/countCard";
-import HordeEnemyCard from "../elements/hordeEnemyCard";
+import EnemyCard from "../elements/EnemyCard";
 import MarketCard from "../elements/marketCard";
 import PlaceholderCard from "../elements/placeholderCard";
 import SceneCard from "../elements/sceneCard";
 
 type Params = {
-  game: Game;
   selectedAbilityCard: AbilityCardIngame | null;
   setSelectedAbilityCard: (abilityCard: AbilityCardIngame | null) => void;
 };
 
 export default function CenterZone(params: Params) {
-  const { game, selectedAbilityCard, setSelectedAbilityCard } = params;
-  const [marketCards, setMarketCards] = useState<MarketCardIngame[]>([]);
-  const [hordeEnemies, setHordeEnemies] = useState<HordeEnemyIngame[]>([]);
+  const { selectedAbilityCard, setSelectedAbilityCard } = params;
   const [sceneCount, setSceneCount] = useState<number>(0);
-  const [warlord, setWarlord] = useState<WarlordIngame | undefined>(undefined);
+  const { game } = useContext(gameContext);
+  const marketCardsInPile = game?.marketCardsInPile;
+  const marketCardsForSale = game?.marketCardsForSale;
+  const enemiesInPile = game?.enemiesInPile;
+  const enemiesFighting = game?.enemiesFighting;
 
   useEffect(() => {
-    const fetchmarketCards = async () => {
-      const response = await axios.get(`/market-cards/${params.game.id}`);
-      setMarketCards(response.data);
-    };
-    const fetchWarlord = async () => {
-      const response = await axios.get(`/warlords/${params.game.id}`);
-      setWarlord(response.data);
-    };
-    const fetchHordeEnemies = async () => {
-      const response = await axios.get(`/horde-enemies/${params.game.id}`);
-      setHordeEnemies(response.data);
-    };
     const fetchSceneCount = async () => {
       // Total number of scenes
       const response = await axios.get("/scenes/count");
       setSceneCount(response.data);
     };
     fetchSceneCount();
-    fetchmarketCards();
-    fetchWarlord();
-    fetchHordeEnemies();
   }, []);
 
   return (
@@ -54,26 +37,21 @@ export default function CenterZone(params: Params) {
         {/* Market cards pile (facing down) */}
         <span className="col-start-1">
           <CountCard
-            count={
-              marketCards.filter((card) => card.location === "MARKET_PILE")
-                .length
-            }
+            count={marketCardsInPile?.length ?? 0}
             zoomDirection="down"
           />
         </span>
         {/* Available market cards (facing up) */}
-        {marketCards
-          .filter((card) => card.location === "MARKET_FOR_SALE")
-          .map((card) => (
-            <MarketCard key={card.id} card={card} />
-          ))}
+        {marketCardsForSale?.map((card) => (
+          <MarketCard key={card.id} card={card} />
+        ))}
       </span>
       <span className="grid grid-cols-6 gap-2 py-1">
         <span className="col-start-3">
-          {/* HordeEnemy pile (facing up), warlord card on top a bit displaced (facing down) */}
+          {/* TODO replace with a flipped warlord card */}
           <PlaceholderCard />
         </span>
-        {game.hasScenes && (
+        {game?.hasScenes && (
           <>
             <span className="col-start-4 col-end-5 transform-gpu rotate-90 translate-x-8">
               {/* Scene pile*/}
@@ -89,16 +67,14 @@ export default function CenterZone(params: Params) {
       <span className="grid grid-cols-6 gap-2 py-1">
         <span> {/* Blank space (first col) */} </span>
         {/* Up to 3 horde enemies and up to 1 Warlord */}
-        {hordeEnemies
-          .filter((enemy) => enemy.hordeEnemyLocation === "FIGHTING")
-          .map((enemy) => (
-            <HordeEnemyCard
-              key={enemy.id}
-              enemy={enemy}
-              selectedAbilityCard={selectedAbilityCard}
-              setSelectedAbilityCard={setSelectedAbilityCard}
-            />
-          ))}
+        {enemiesFighting?.map((enemy) => (
+          <EnemyCard
+            key={enemy.id}
+            enemyIngame={enemy}
+            selectedAbilityCard={selectedAbilityCard}
+            setSelectedAbilityCard={setSelectedAbilityCard}
+          />
+        ))}
       </span>
     </div>
   );
