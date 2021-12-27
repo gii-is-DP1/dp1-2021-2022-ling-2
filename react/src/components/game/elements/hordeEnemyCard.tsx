2292ff9ba4f6201b8df9226c3279d34850925915
andrsdt <andresdt@pm.me>
2021-12-27 02:15:22 +0100

    add: frontend attack enemy

--- react/src/components/game/elements/hordeEnemyCard.tsx
+++ react/src/components/game/elements/hordeEnemyCard.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import toast from "react-hot-toast";
+import { useParams } from "react-router";
+import axios from "../../../api/axiosConfig";
 import { BASE_IMAGE_PATH } from "../../../constants/paths";
+import UserContext from "../../../context/user";
 import { AbilityCardIngame } from "../../../interfaces/AbilityCardIngame";
 import { HordeEnemyIngame } from "../../../interfaces/HordeEnemyIngame";
+import GameContext from "../../../context/game";
 
 type Params = {
   enemy: HordeEnemyIngame;
@@ -12,12 +18,24 @@ type Params = {
 export default function HordeEnemyCard(params: Params) {
   const { enemy, flipped, selectedAbilityCard, setSelectedAbilityCard } =
     params;
+  const { userToken } = useContext(UserContext);
+  const { gameId } = useParams<{ gameId: string }>(); // get params from react router link
+  const { setGame } = useContext(GameContext);
 
-  const handleOnClick = () => {
+  const handleOnClick = async () => {
     if (!selectedAbilityCard) return; // Do nothing if there is no selected abilityCard
     setSelectedAbilityCard(null);
-
-    // playCard(card, enemy);
+    try {
+      const response = await axios.post(
+        `/games/${gameId}/ability-cards/${selectedAbilityCard.id}`,
+        { enemyId: enemy.id }, // Payload
+        { headers: { Authorization: "Bearer " + userToken } }
+      );
+      const _game = response.data;
+      setGame(_game);
+    } catch (error: any) {
+      toast.error(error?.message);
+    }
   };
 
   return (
