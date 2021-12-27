1635f552071e227c97a1842e8437739615ab4e3a
Pablosancval <pablosancval@gmail.com>
2021-12-27 22:53:51 +0100

    addition of a couple of cards
    addition of saqueoOro and saqueoOroGloria, the getter of number of enemies on table still todo

--- spring-boot/src/main/java/org/springframework/ntfh/cardlogic/abilitycard/rogue/SaqueoOroGloria.java
+++ spring-boot/src/main/java/org/springframework/ntfh/cardlogic/abilitycard/rogue/SaqueoOroGloria.java
@@ -1,12 +1,14 @@
 package org.springframework.ntfh.cardlogic.abilitycard.rogue;
 
+import org.springframework.ntfh.command.GetGloryCommand;
 import org.springframework.ntfh.command.GetGoldCommand;
 import org.springframework.ntfh.entity.player.Player;
 
-public class SaqueoOro {
+public class SaqueoOroGloria {
     public void execute(Player playerFrom){
         Integer nEnemies = 0;
         //TODO enemies on table
-        new GetGoldCommand(nEnemies*2, playerFrom).execute();
+        new GetGoldCommand(nEnemies, playerFrom).execute();
+        new GetGloryCommand(nEnemies, playerFrom).execute();
     }
 }
