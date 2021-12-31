68b7455bfa6d99e54ae44685176ea7741ca994e9
alegestor <alegestor@alum.us.es>
2021-12-31 12:46:21 +0100

    Pal james

--- spring-boot/src/test/java/org/springframework/ntfh/user/UserControllerTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/user/UserControllerTest.java
@@ -1,121 +1,53 @@
 package org.springframework.ntfh.user;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import org.hamcrest.Matchers;
-
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.ntfh.configuration.SecurityConfiguration;
-import org.springframework.ntfh.entity.user.User;
-import org.springframework.ntfh.entity.user.UserController;
-import org.springframework.ntfh.entity.user.UserService;
-import org.springframework.ntfh.entity.user.authorities.AuthoritiesService;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.ntfh.entity.user.User;
+import org.springframework.ntfh.entity.user.UserController;
+import org.springframework.ntfh.entity.user.UserService;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
- * @author alegestor
- */
+* @author alegestor 
+*/
 
-// @WebMvcTest(controllers=UserController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
-// 		excludeAutoConfiguration= SecurityConfiguration.class)
-// @WebMvcTest(UserController.class)
-// @RunWith(SpringRunner.class)
-// @SpringBootTest
-// @AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
 
-    @InjectMocks
-    UserController userController;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
-    UserService userService;
+    @MockBean
+    private UserService userService;
+
+    private static ObjectMapper mapper = new ObjectMapper(); 
+
+    @Test
+    void testGetByUsername() throws Exception {
+        List<User> users = new ArrayList<>();
+        User tester = new User();
+        tester.setUsername("antonio");
+        tester.setPassword("antonio");
+        tester.setEmail("antonio@mail.com");
+        users.add(tester);
+        Mockito.when(userService.findAll()).thenReturn(users);
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].username", Matchers.equalTo("antonio")));
+    }
     
-    // private static final String TEST_USER_USERNAME = "antonio";
-
-    // @Autowired
-    // private MockMvc mockMvc;
-
-    // @MockBean
-    // private UserService userService;
-
-    // @MockBean
-    // private AuthoritiesService authoritiesService;
-
-    // private User tester;
-
-    // private static ObjectMapper mapper = new ObjectMapper();
-
-
-    
-    // @BeforeEach
-    // void setup() {
-    //     tester = new User();
-    //     tester.setUsername(TEST_USER_USERNAME);
-    //     tester.setPassword("antonio");
-    //     tester.setEmail("antonio@mail.com");
-    //     given(this.userService.findUser(TEST_USER_USERNAME)).willReturn(Optional.of(tester));
-    // }
-
-    // Error 403
-    // @Test
-    // void testGetByUsername() throws Exception {
-    //     List<User> users = new ArrayList<>();
-    //     tester = new User();
-    //     tester.setUsername("antonio");
-    //     tester.setPassword("antonio");
-    //     tester.setEmail("antonio@mail.com");
-    //     users.add(tester);
-    //     Mockito.when(userService.findAll()).thenReturn(users);
-    //     mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
-    //             .andExpect(jsonPath("$[0].username", Matchers.equalTo("antonio")));
-    // }
-
-        @Test
-        void testGetAllUsers() {
-            List<User> users = new ArrayList<>();
-            User antonio = new User();
-            antonio.setUsername("antonio");
-            antonio.setPassword("antonio");
-            antonio.setEmail("antonio@mail.com");
-            users.add(antonio);
-            User eustaquio = new User();
-            eustaquio.setUsername("eustaquio");
-            eustaquio.setPassword("eustaquio");
-            eustaquio.setEmail("eustaquio@mail.com");
-            users.add(eustaquio);
-
-            when(userService.findAll()).thenReturn(users);
-            
-            List<User> res = new ArrayList<>();
-            res = userController.getUser("antonio");
-        }
-
 }
---
+++ spring-boot/src/test/java/org/springframework/ntfh/user/UserControllerTestDP.java
@@ -0,0 +1,71 @@
+package org.springframework.ntfh.user;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.ntfh.configuration.SecurityConfiguration;
+import org.springframework.ntfh.entity.user.User;
+import org.springframework.ntfh.entity.user.UserController;
+import org.springframework.ntfh.entity.user.UserService;
+import org.springframework.ntfh.entity.user.authorities.Authorities;
+import org.springframework.ntfh.entity.user.authorities.AuthoritiesService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author alegestor
+ */
+
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// @AutoConfigureMockMvc
+@WebMvcTest(controllers = UserController.class, excludeFilters = @ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
+public class UserControllerTestDP {
+    
+    private static final String TESTER = "antonio";
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private AuthoritiesService authoritiesService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private User antonio;
+
+    @BeforeEach
+    void setup() {
+        antonio = new User();
+        antonio.setUsername(TESTER);
+        antonio.setPassword("antonio");
+        antonio.setEmail("antonio@mail.com");
+        // authoritiesService.saveAuthorities(TESTER, "user");
+        given(this.userService.findUser(TESTER)).willReturn(antonio);
+    }
+    
+    @WithMockUser(value = "spring")
+    @Test
+    void testGetByUsername() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(model().attributeExists("user"));
+    }
+}
