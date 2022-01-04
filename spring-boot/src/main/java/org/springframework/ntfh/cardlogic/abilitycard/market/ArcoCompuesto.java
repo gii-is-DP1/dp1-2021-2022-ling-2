c08ebc0a4f60631116f5b8ef4fb01397b9b32086
andrsdt <andresdt@pm.me>
2022-01-04 02:34:19 +0100

    refactor: replace iterator with stream

--- spring-boot/src/main/java/org/springframework/ntfh/cardlogic/abilitycard/market/ArcoCompuesto.java
+++ spring-boot/src/main/java/org/springframework/ntfh/cardlogic/abilitycard/market/ArcoCompuesto.java
@@ -1,11 +1,10 @@
 package org.springframework.ntfh.cardlogic.abilitycard.market;
 
-import java.util.Iterator;
+import java.util.List;
 
 import org.springframework.ntfh.command.DealDamageCommand;
 import org.springframework.ntfh.entity.enemy.ingame.EnemyIngame;
 import org.springframework.ntfh.entity.player.Player;
-import org.springframework.ntfh.entity.proficiency.Proficiency;
 import org.springframework.ntfh.entity.proficiency.ProficiencyTypeEnum;
 import org.springframework.stereotype.Component;
 
@@ -18,16 +17,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ArcoCompuesto {
     public void execute(Player playerFrom, EnemyIngame targetedEnemy) {
-        // TODO replace iterator
-        Iterator<Proficiency> iterador = playerFrom.getCharacterType().getProficiencies().iterator();
-        Integer damageModifier = 0;
-        while (iterador.hasNext()) {
-            ProficiencyTypeEnum tipo = iterador.next().getProficiencyTypeEnum();
-            if (tipo.equals(ProficiencyTypeEnum.RANGED)) {
-                damageModifier = iterador.next().getSecondaryDebuff();
-                break;
-            }
-        }
-        new DealDamageCommand(4 - damageModifier, targetedEnemy).execute();
+        List<ProficiencyTypeEnum> neededProfs = List.of(ProficiencyTypeEnum.RANGED);
+        Integer damageModifier = playerFrom.getCharacterType().getProficiencies().stream()
+                .filter(proficiency -> neededProfs.contains(proficiency.getProficiencyTypeEnum()))
+                .findFirst().orElseThrow().getSecondaryDebuff();
+        new DealDamageCommand(4 + damageModifier, targetedEnemy).execute();
     }
 }
