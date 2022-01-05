af5d2b2e4b81a1a3dc51591b03288b9111bac752
Pablosancval <pablosancval@gmail.com>
2022-01-05 17:58:15 +0100

    eliminated useless imports

--- spring-boot/src/main/java/org/springframework/ntfh/cardlogic/abilitycard/rogue/BallestaPrecisa.java
+++ spring-boot/src/main/java/org/springframework/ntfh/cardlogic/abilitycard/rogue/BallestaPrecisa.java
@@ -1,11 +1,8 @@
 package org.springframework.ntfh.cardlogic.abilitycard.rogue;
 
 import org.springframework.ntfh.command.DealDamageCommand;
-import org.springframework.ntfh.entity.character.CharacterTypeEnum;
 import org.springframework.ntfh.entity.enemy.ingame.EnemyIngame;
-import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCard;
 import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCardTypeEnum;
-import org.springframework.ntfh.entity.playablecard.abilitycard.ingame.AbilityCardIngame;
 import org.springframework.ntfh.entity.player.Player;
 import org.springframework.stereotype.Component;
 
