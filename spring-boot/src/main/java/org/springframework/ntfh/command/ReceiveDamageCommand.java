cf0dfb1d3f3fed0bfb8135bf130b4e7e8aacf9f5
andrsdt <andresdt@pm.me>
2022-01-05 18:13:35 +0100

    fix: don't deal damage if restrained

--- spring-boot/src/main/java/org/springframework/ntfh/command/ReceiveDamageCommand.java
+++ spring-boot/src/main/java/org/springframework/ntfh/command/ReceiveDamageCommand.java
@@ -1,5 +1,6 @@
 package org.springframework.ntfh.command;
 
+import org.springframework.ntfh.entity.enemy.ingame.EnemyIngame;
 import org.springframework.ntfh.entity.playablecard.abilitycard.ingame.AbilityCardIngame;
 import org.springframework.ntfh.entity.player.Player;
 
@@ -10,20 +11,27 @@ public class ReceiveDamageCommand implements Command {
 
     private Integer damage;
 
+    private EnemyIngame enemyFrom;
+
     private Player playerTo;
 
     @Override
     public void execute() {
+        if (enemyFrom.getRestrained())
+            damage = 0;
+
         for (int i = 0; i < damage; i++) {
-            Boolean noCardsLeft = playerTo.getAbilityPile().isEmpty();
-            if (noCardsLeft) {
-                new GiveWoundCommand(playerTo);
-                break;
-            }
+            // "Top card" is the last card in the abilityPile list
             Integer topCardIndex = playerTo.getAbilityPile().size() - 1;
             AbilityCardIngame topCard = playerTo.getAbilityPile().get(topCardIndex);
+
             playerTo.getAbilityPile().remove(topCard);
             playerTo.getDiscardPile().add(topCard);
+
+            if (playerTo.getAbilityPile().isEmpty()) {
+                new GiveWoundCommand(playerTo).execute();
+                break;
+            }
         }
     }
 
