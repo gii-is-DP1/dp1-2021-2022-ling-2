7aec0e8d44a75cb208ecced357f740ae13b32990
Jaime Stockwell Mendoza <jstockwellmendoza@gmail.com>
2022-01-06 01:12:03 +0100

    feat: branch creation

--- spring-boot/src/main/java/org/springframework/ntfh/NtfhApplication.java
+++ spring-boot/src/main/java/org/springframework/ntfh/NtfhApplication.java
@@ -3,11 +3,15 @@ package org.springframework.ntfh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication()
 public class NtfhApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NtfhApplication.class, args);
+		log.info("NTFH Application started");
 	}
 
 }
---
+++ spring-boot/src/main/java/org/springframework/ntfh/configuration/LoggingController.java
@@ -0,0 +1,22 @@
+package org.springframework.ntfh.configuration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class LoggingController {
+    
+    @RequestMapping("/logger")
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+ 
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
