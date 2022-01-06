3be9a58b7cb96c3fcc2788c0d21b7be07bbcd4e8
alegestor <alegestor@alum.us.es>
2022-01-06 13:48:30 +0100

    Update MarketCardServiceTest.java

--- spring-boot/src/test/java/org/springframework/ntfh/playablecard/marketcard/MarketCardServiceTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/playablecard/marketcard/MarketCardServiceTest.java
@@ -37,7 +37,7 @@ public class MarketCardServiceTest {
 
     @Test
     public void testfindById() {
-        MarketCard tester = this.marketCardService.findMarketCardById(6).orElse(null);
+        MarketCard tester = this.marketCardService.findMarketCardById(6).get();
         assertEquals(MarketCardTypeEnum.PIEDRA_DE_AMOLAR, tester.getMarketCardTypeEnum());
         assertEquals(4, tester.getPrice());
     }
