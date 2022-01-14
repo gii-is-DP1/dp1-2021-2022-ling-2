e6ac2ad302d779188170e7074d6799b7483b90d8
andrsdt <andresdt@pm.me>
2022-01-15 00:24:57 +0100

    add: frontend support for new attributes

--- react/src/pages/game.tsx
+++ react/src/pages/game.tsx
@@ -67,8 +67,9 @@ export default function Game() {
   const fetchGame = async () => {
     try {
       const response = await axios.get(`/games/${gameId}`);
-      const _game = response.data;
+      const _game: IGame = response.data;
       setGame(_game);
+      if (_game.finishTime) history.push(ROUTES.HOME); // TODO redirect to endgame summary
     } catch (error: any) {
       toast.error(error?.message);
       if (error?.status >= 400) history.push(ROUTES.BROWSE_LOBBIES);
@@ -145,11 +146,11 @@ export default function Game() {
     const fetchTurn = async () => {
       try {
         const response = await axios.get(`/games/${gameId}/turn`);
-        const _turn = response.data;
+        const _turn: Turn = response.data;
         setTurn(_turn);
 
         // Fetch the game if it's not my turn, or if it's the first time the turn has changed to me
-        if (!isPlayersTurn(_turn, loggedUser.username)) {
+        if (!isPlayersTurn(_turn, loggedUser.username) || _turn.player.dead) {
           fetchGame();
           setIsNewTurn(true);
         } else {
