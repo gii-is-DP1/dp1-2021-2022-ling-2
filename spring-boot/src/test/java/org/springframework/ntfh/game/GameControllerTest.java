5a59f166beb105d42712fe4f559c4fcc30d61f7c
alegestor <alegestor@alum.us.es>
2022-01-16 22:18:04 +0100

    Update GameControllerTest.java

--- spring-boot/src/test/java/org/springframework/ntfh/game/GameControllerTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/game/GameControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.ntfh.entity.game.Game;
 import org.springframework.ntfh.entity.game.GameService;
+import org.springframework.ntfh.entity.lobby.Lobby;
 import org.springframework.ntfh.util.TokenUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -38,6 +39,8 @@ public class GameControllerTest {
 
     static Game game1, game2;
 
+    static Lobby lobby;
+
     @BeforeAll
     static void setup() {
         game1 = new Game();
@@ -45,6 +48,9 @@ public class GameControllerTest {
 
         game2 = new Game();
         game2.setId(2);
+
+        lobby = new Lobby();
+        // lobby.setId(1);
     }
 
     @Test
@@ -59,7 +65,8 @@ public class GameControllerTest {
     @Disabled
     @Test
     void testCreateGame() throws Exception {
-        final String POST_JSON = "{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2,\"users\":[{\"username\":\"user2\",\"email\":\"user2@mail.com\",\"enabled\":true,\"lobby\":{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2},\"player\":null,\"character\":{\"id\":3,\"baseHealth\":2,\"characterTypeEnum\":\"ROGUE\",\"characterGenderEnum\":\"MALE\",\"proficiencies\":[{\"id\":1,\"proficiencyTypeEnum\":\"DEXTERITY\",\"secondaryDebuff\":0},{\"id\":5,\"proficiencyTypeEnum\":\"RANGED\",\"secondaryDebuff\":-1}]},\"authorities\":[{\"id\":16,\"authority\":\"user\"}]},{\"username\":\"user1\",\"email\":\"user1@mail.com\",\"enabled\":true,\"lobby\":{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2},\"player\":null,\"character\":{\"id\":7,\"baseHealth\":2,\"characterTypeEnum\":\"WIZARD\",\"characterGenderEnum\":\"MALE\",\"proficiencies\":[{\"id\":6,\"proficiencyTypeEnum\":\"SPELL\",\"secondaryDebuff\":0}]},\"authorities\":[{\"id\":15,\"authority\":\"user\"}]}],\"leader\":null,\"host\":{\"username\":\"user1\",\"email\":\"user1@mail.com\",\"enabled\":true,\"lobby\":{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2},\"player\":null,\"character\":{\"id\":7,\"baseHealth\":2,\"characterTypeEnum\":\"WIZARD\",\"characterGenderEnum\":\"MALE\",\"proficiencies\":[{\"id\":6,\"proficiencyTypeEnum\":\"SPELL\",\"secondaryDebuff\":0}]},\"authorities\":[{\"id\":15,\"authority\":\"user\"}]}}";
+        when(gameService.createFromLobby(lobby)).thenReturn(game1);
+        final String POST_JSON = "{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2,\"users\":[{\"username\":\"user2\",\"email\":\"user2@mail.com\",\"enabled\":true,\"lobby\":{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2},\"player\":null,\"character\":{\"id\":5,\"baseHealth\":3,\"characterTypeEnum\":\"WARRIOR\",\"characterGenderEnum\":\"MALE\",\"proficiencies\":[{\"id\":2,\"proficiencyTypeEnum\":\"MELEE\",\"secondaryDebuff\":0}]},\"authorities\":[{\"id\":16,\"authority\":\"user\"}]},{\"username\":\"user1\",\"email\":\"user1@mail.com\",\"enabled\":true,\"lobby\":{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2},\"player\":null,\"character\":{\"id\":1,\"baseHealth\":3,\"characterTypeEnum\":\"RANGER\",\"characterGenderEnum\":\"MALE\",\"proficiencies\":[{\"id\":4,\"proficiencyTypeEnum\":\"RANGED\",\"secondaryDebuff\":0},{\"id\":3,\"proficiencyTypeEnum\":\"MELEE\",\"secondaryDebuff\":-1}]},\"authorities\":[{\"id\":15,\"authority\":\"user\"}]}],\"leader\":null,\"host\":{\"username\":\"user1\",\"email\":\"user1@mail.com\",\"enabled\":true,\"lobby\":{\"id\":4,\"name\":\"user1's game\",\"game\":null,\"hasScenes\":false,\"spectatorsAllowed\":false,\"maxPlayers\":2},\"player\":null,\"character\":{\"id\":1,\"baseHealth\":3,\"characterTypeEnum\":\"RANGER\",\"characterGenderEnum\":\"MALE\",\"proficiencies\":[{\"id\":4,\"proficiencyTypeEnum\":\"RANGED\",\"secondaryDebuff\":0},{\"id\":3,\"proficiencyTypeEnum\":\"MELEE\",\"secondaryDebuff\":-1}]},\"authorities\":[{\"id\":15,\"authority\":\"user\"}]}}";
         mockMvc.perform(post("/games").contentType(MediaType.APPLICATION_JSON)
         .header("authorization", "Bearer " + TokenUtils.ADMIN_TOKEN)
         .content(POST_JSON))
