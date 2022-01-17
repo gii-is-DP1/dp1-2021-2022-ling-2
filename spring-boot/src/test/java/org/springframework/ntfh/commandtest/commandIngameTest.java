a0ed7db6f6b4e3be6f8bdb26a8bd7b8d1939389c
Pablosancval <pablosancval@gmail.com>
2022-01-17 13:22:29 +0100

    Update commandIngameTest.java
    added test attack phase command
    added test change enemy command
    added test discard command
    added test draw command
    added test exile command

--- spring-boot/src/test/java/org/springframework/ntfh/commandtest/commandIngameTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/commandtest/commandIngameTest.java
@@ -1,8 +1,9 @@
 package org.springframework.ntfh.commandtest;
 
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.ntfh.command.AttackPhaseEndCommand;
+import org.springframework.ntfh.command.ChangeEnemyCommand;
 import org.springframework.ntfh.command.DealDamageCommand;
+import org.springframework.ntfh.command.DiscardCommand;
+import org.springframework.ntfh.command.DrawCommand;
+import org.springframework.ntfh.command.ExileCommand;
 import org.springframework.ntfh.entity.character.CharacterService;
 import org.springframework.ntfh.entity.enemy.EnemyService;
 import org.springframework.ntfh.entity.enemy.ingame.EnemyIngame;
@@ -21,12 +27,17 @@ import org.springframework.ntfh.entity.game.Game;
 import org.springframework.ntfh.entity.game.GameService;
 import org.springframework.ntfh.entity.lobby.Lobby;
 import org.springframework.ntfh.entity.lobby.LobbyService;
+import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCard;
 import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCardService;
+import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCardTypeEnum;
+import org.springframework.ntfh.entity.playablecard.abilitycard.ingame.AbilityCardIngame;
 import org.springframework.ntfh.entity.playablecard.abilitycard.ingame.AbilityCardIngameService;
 import org.springframework.ntfh.entity.playablecard.marketcard.MarketCardService;
 import org.springframework.ntfh.entity.playablecard.marketcard.ingame.MarketCardIngameService;
 import org.springframework.ntfh.entity.player.Player;
+import org.springframework.ntfh.entity.turn.Turn;
 import org.springframework.ntfh.entity.turn.TurnService;
+import org.springframework.ntfh.entity.turn.TurnStateType;
 import org.springframework.ntfh.entity.turn.concretestates.MarketState;
 import org.springframework.ntfh.entity.turn.concretestates.PlayerState;
 import org.springframework.ntfh.entity.user.User;
@@ -78,7 +89,7 @@ public class commandIngameTest {
 
     protected Lobby lobbyTester;
 
-    private Player playerTester;
+    private Player ranger;
 
     @BeforeEach
     public void init() {
@@ -101,7 +112,7 @@ public class commandIngameTest {
 
         gameTester = gameService.createFromLobby(lobbyTester);
         user1.setLobby(lobbyTester);
-        playerTester = gameTester.getPlayers().get(0);
+        ranger = gameTester.getPlayers().get(0);
     }
 
     @AfterEach
@@ -109,15 +120,78 @@ public class commandIngameTest {
         gameService.delete(gameTester);
     }
 
+    @Test
+    void testAttackPhaseEndCommand(){
+        turnService.initializeFromGame(gameTester);
+        Integer gameId = gameTester.getId();
+        Turn initialTurn = gameService.getCurrentTurnByGameId(gameId);
+        TurnStateType initialTurnState = initialTurn.getStateType();
+        new AttackPhaseEndCommand(gameService, ranger).execute();
+
+        assertThat(initialTurn.getStateType()).isNotEqualTo(initialTurnState);
+    }
+
+    @Test
+    void testChangeEnemyCommand(){
+        enemyIngameService.initializeFromGame(gameTester);
+        List<EnemyIngame> initialEnemiesFighting = gameTester.getEnemiesFighting();
+        EnemyIngame changedEnemy = initialEnemiesFighting.get(0);
+        new ChangeEnemyCommand(ranger, changedEnemy).execute();
+        List<EnemyIngame> currentEnemiesFighting = gameTester.getEnemiesFighting();
+
+        assertThat(currentEnemiesFighting).doesNotContain(changedEnemy);
+    }
 
     @Test
     void testDealDamageCommand(){
         EnemyIngame enemyIngame = enemyIngameService.createFromEnemy(enemyService.findEnemyById(12).get(), gameTester);
-        turnService.initializeFromGame(gameTester);
         Integer initialEndurance = enemyIngame.getCurrentEndurance();
-        new DealDamageCommand(1, playerTester, enemyIngame).execute();
+        new DealDamageCommand(1, ranger, enemyIngame).execute();
 
         assertThat(enemyIngame.getCurrentEndurance()).isEqualTo(initialEndurance-1);
+    }
+
+    @Test
+    void testDiscardCommand(){
+        turnService.initializeFromGame(gameTester);
+        Integer initialDiscardedCards = ranger.getDiscardPile().size();
+        new DiscardCommand(1, ranger).execute();
+        Integer discardedCards = ranger.getDiscardPile().size();
+
+        assertThat(initialDiscardedCards).isZero();
+        assertThat(discardedCards).isEqualTo(1);
+
+    }
+
+    @Test
+    void testDrawCommand(){
+        turnService.initializeFromGame(gameTester);
+        Integer initialHand = ranger.getHand().size();
+        new DrawCommand(1, ranger).execute();
+        Integer currentHand = ranger.getHand().size();
+
+        assertThat(initialHand).isEqualTo(4);
+        assertThat(currentHand).isEqualTo(5);
+    }
+
+
+    @Test
+    void testExileCommand(){
+        turnService.initializeFromGame(gameTester);
+        AbilityCard pocionCurativa = abilityCardService.findById(62);
+        AbilityCardIngame pocionCurativaIngame = abilityCardIngameService.createFromAbilityCard(pocionCurativa, ranger);
+        List<AbilityCardIngame> currentHand = ranger.getHand();
+        currentHand.add(pocionCurativaIngame);
+        ranger.setHand(currentHand);
+        Integer currentAbilityPile = ranger.getAbilityPile().size();
+
+        assertThat(currentHand.size()).isEqualTo(5);
+        assertThat(ranger.getDiscardPile().size()).isZero();
+
+        new ExileCommand(ranger, AbilityCardTypeEnum.POCION_CURATIVA).execute();
+
+        assertThat(currentHand.size()).isEqualTo(4);
+        assertThat(ranger.getDiscardPile().size()).isZero();
     }
 
     
