284e803a0b2298a030add4fd4ef8e3db78dd9f83
andrsdt <andresdt@pm.me>
2022-01-17 20:42:01 +0100

    fix: missing autowired

--- spring-boot/src/main/java/org/springframework/ntfh/entity/character/CharacterService.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/character/CharacterService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CharacterService {
 
+    @Autowired
     private CharacterRepository characterRepository;
 
     public Character findById(Integer id) throws DataAccessException {
