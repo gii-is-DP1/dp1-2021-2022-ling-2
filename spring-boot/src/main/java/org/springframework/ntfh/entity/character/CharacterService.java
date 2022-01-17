c30936b6b010c0577dbbd37daeb4b9579323ef83
andrsdt <andresdt@pm.me>
2022-01-17 14:37:00 +0100

    refactor: autowired in constructor

--- spring-boot/src/main/java/org/springframework/ntfh/entity/character/CharacterService.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/character/CharacterService.java
@@ -1,15 +1,18 @@
 package org.springframework.ntfh.entity.character;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CharacterService {
 
-    @Autowired
     private CharacterRepository characterRepository;
+
+    @Autowired
+    public CharacterService(CharacterRepository characterRepository) {
+        this.characterRepository = characterRepository;
+    }
 
     public Optional<Character> findCharacterById(Integer id) {
         return characterRepository.findById(id);
