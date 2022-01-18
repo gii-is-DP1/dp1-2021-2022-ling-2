62a2d94db6fe4a8dec9d81d5547921fe1365a7e5
andrsdt <andresdt@pm.me>
2022-01-18 23:13:58 +0100

    refactor: remove unused methods

--- spring-boot/src/main/java/org/springframework/ntfh/entity/player/PlayerService.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/player/PlayerService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.ntfh.entity.character.Character;
 import org.springframework.ntfh.entity.character.CharacterService;
-import org.springframework.ntfh.entity.lobby.Lobby;
 import org.springframework.ntfh.entity.user.User;
 import org.springframework.ntfh.entity.user.UserService;
 import org.springframework.stereotype.Service;
@@ -57,39 +56,6 @@ public class PlayerService {
         if (!player.isPresent())
             throw new DataAccessException("Player with id " + playerId + " was not found") {};
         return player.get();
-    }
-
-    /**
-     * Creates a new player with the given user information and lobby that the player will be created from.
-     * 
-     * @param user
-     * @param lobby
-     * @return
-     */
-    @Transactional
-    public Player createFromUser(User user, Lobby lobby, Integer turnOrder) {
-        Player player = new Player();
-        player.setGlory(0);
-        player.setKills(0);
-        player.setGold(0);
-        player.setWounds(0);
-        player.setGuard(0);
-        // player.setTurnOrder(turnOrder);
-
-        // ! handle this before starting a game
-        // if (user.getCharacter() == null) {
-        // throw new IllegalArgumentException("User " + user.getUsername() + " has not selected a character");
-        // }
-
-        player.setUser(user);
-        Player playerDB = playerRepository.save(player);
-
-        user.setPlayer(player);
-        userService.save(user);
-
-        log.info("Player created by user " + user.getUsername() + " in lobby id " + lobby.getId());
-        return playerDB;
-
     }
 
     /**
