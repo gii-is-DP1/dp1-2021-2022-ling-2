2a3a5e69fc438c704f4e167e128c13f184bf266a
alegestor <alegestor@alum.us.es>
2022-01-18 13:51:37 +0100

    Update SceneServiceTest.java

--- spring-boot/src/test/java/org/springframework/ntfh/scene/SceneServiceTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/scene/SceneServiceTest.java
@@ -1,6 +1,6 @@
 package org.springframework.ntfh.scene;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +27,13 @@ public class SceneServiceTest {
         // TODO: Delete all and create mock initial data. Then test count.
         // By doing this we will make this test independent of the initial data.
         Integer count = sceneService.count();
-        assertEquals(12, count);
+        assertThat(count).isEqualTo(12);
     }
 
     @Test
     public void testfindById() {
         Scene tester = this.sceneService.findSceneById(8).orElse(null);
-        assertEquals(SceneTypeEnum.PORTAL_DE_ULTHAR, tester.getSceneTypeEnum());
+        assertThat(tester.getSceneTypeEnum()).isEqualTo(SceneTypeEnum.PORTAL_DE_ULTHAR);
     }
 
 }
