9e39a37bd07affdd994fe9ca172a5bccff772fd8
alegestor <alegestor@alum.us.es>
2022-01-18 13:49:33 +0100

    Update ProficiencyServiceTest.java

--- spring-boot/src/test/java/org/springframework/ntfh/proficiency/ProficiencyServiceTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/proficiency/ProficiencyServiceTest.java
@@ -1,6 +1,6 @@
 package org.springframework.ntfh.proficiency;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
@@ -23,22 +23,27 @@ public class ProficiencyServiceTest {
     @Autowired
     private ProficiencyService proficiencyService;
 
+    protected Integer ALL_PROFICIENCIES = 6;
+
     @Test
     void testCountWithInitialData() {
         Integer counter = proficiencyService.count();
-        assertEquals(6, counter);
+
+        assertThat(counter).isEqualTo(ALL_PROFICIENCIES);
     }
 
     @Test
     void testFindAll() {
         Integer counter = Lists.newArrayList(proficiencyService.findAll()).size();
-        assertEquals(6, counter);
+
+        assertThat(counter).isEqualTo(ALL_PROFICIENCIES);
     }
 
     @Test
     void testFindProficiencyById() {
         Proficiency tester = proficiencyService.findProficiencyById(2).get();
-        assertEquals(ProficiencyTypeEnum.MELEE, tester.getProficiencyTypeEnum());
+
+        assertThat(tester.getProficiencyTypeEnum()).isEqualByComparingTo(ProficiencyTypeEnum.MELEE);
     }
     
 }
