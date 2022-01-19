5aec2c0cdb8a23d4767d6b923520419eebdaa55c
alegestor <alegestor@alum.us.es>
2022-01-19 11:50:02 +0100

    Update AbilityCardIngameService.java

--- spring-boot/src/main/java/org/springframework/ntfh/entity/playablecard/abilitycard/ingame/AbilityCardIngameService.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/playablecard/abilitycard/ingame/AbilityCardIngameService.java
@@ -94,7 +94,7 @@ public class AbilityCardIngameService {
      * @param player
      */
     public void refillHandWithCards(Player player) {
-        if (player.isDead())
+        if (Boolean.TRUE.equals(player.isDead()))
             return;
 
         List<AbilityCardIngame> playerAbilityPile = player.getAbilityPile();
