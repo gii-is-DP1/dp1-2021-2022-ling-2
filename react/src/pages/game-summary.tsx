b9ce8476044fa3e93b121b472197115d036f3ae8
andrsdt <andresdt@pm.me>
2022-01-20 17:04:17 +0100

    fix: game ranking visualization

--- react/src/pages/game-summary.tsx
+++ react/src/pages/game-summary.tsx
@@ -34,8 +34,8 @@ export default function GameSummary() {
 
   const rankingSort = (p1: Player, p2: Player): number => {
     // Sorting criteria for deciding the winner: Sort by glory. If equal, sort by kills.
-    const compareGlory = p1.glory - p2.glory;
-    return compareGlory === 0 ? p1.kills - p2.kills : compareGlory;
+    const compareGlory = p2.glory - p1.glory;
+    return compareGlory === 0 ? p2.kills - p1.kills : compareGlory;
   };
 
   return (
