4f372d1ef4dfee1090be37cf44b31b484bd44e40
andrsdt <andresdt@pm.me>
2022-01-20 03:17:41 +0100

    add: more stats custom queries

--- spring-boot/src/main/java/org/springframework/ntfh/entity/achievement/AchievementController.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/achievement/AchievementController.java
@@ -1,8 +1,12 @@
 package org.springframework.ntfh.entity.achievement;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ntfh.entity.user.UserRepository;
+import org.springframework.ntfh.entity.game.Game;
+import org.springframework.ntfh.entity.game.GameService;
+import org.springframework.ntfh.entity.statistics.StatisticsService;
+import org.springframework.ntfh.entity.user.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/achievements")
 public class AchievementController {
+
     @Autowired
     private AchievementService achievementService;
 
     @Autowired
-    UserRepository userRepository;
+    private StatisticsService statisticsService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -34,6 +39,14 @@ public class AchievementController {
     @GetMapping("types")
     @ResponseStatus(HttpStatus.OK)
     public Iterable<AchievementType> getTypes() {
+        // ! PROVISIONAL PLACE FOR TESTING STATISTICS
+        Game longest = statisticsService.longestGame();
+        Game shortest = statisticsService.shortestGame();
+        List<User> fiveMoreWon = statisticsService.rankingByWonGames();
+        List<Object> fiveMoreKills = statisticsService.rankingByTotalKills();
+        List<Object> fiveMoreGlory = statisticsService.rankingByTotalGlory();
+        Double averageNumPlayers = statisticsService.averagePlayersPerGame();
+
         return this.achievementService.findAllTypes();
     }
 
--- spring-boot/src/main/java/org/springframework/ntfh/entity/statistics/StatisticsService.java
+++ spring-boot/src/main/java/org/springframework/ntfh/entity/statistics/StatisticsService.java
@@ -1,8 +1,10 @@
 package org.springframework.ntfh.entity.statistics;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.ntfh.entity.game.Game;
 import org.springframework.ntfh.entity.game.GameRepository;
 import org.springframework.ntfh.entity.game.GameStateType;
 import org.springframework.ntfh.entity.user.User;
@@ -30,5 +32,52 @@ public class StatisticsService {
     public User maxNumberOfGamesWon() {
         Pageable firstResult = PageRequest.of(0, 1);
         return userRepository.maxNumberOfGamesWon(firstResult).get(0);
+    }
+
+    public Game longestGame() {
+        Pageable firstResult = PageRequest.of(0, 1);
+        return gameRepository.gameWithMaxDuration(GameStateType.FINISHED, firstResult).get(0);
+    }
+
+    public Game shortestGame() {
+        Pageable firstResult = PageRequest.of(0, 1);
+        return gameRepository.gameWithMinDuration(GameStateType.FINISHED, firstResult).get(0);
+    }
+
+    public Integer totalGameDuration() {
+        // TODO: implement
+        return null;
+    }
+
+    public Integer averageGameDuration() {
+        // TODO: implement
+        return null;
+    }
+
+    public Double averagePlayersPerGame() {
+        // ! NOT WORKING
+        // return gameRepository.averagePlayersPerGame();
+        return null;
+    }
+
+    public Integer averageTurnsPerGame() {
+        // TODO: implement
+        // Personal idea
+        return null;
+    }
+
+    public List<User> rankingByWonGames() {
+        Pageable topFive = PageRequest.of(0, 5);
+        return userRepository.rankingByWonGames(topFive);
+    }
+
+    public List<Object> rankingByTotalGlory() {
+        Pageable topFive = PageRequest.of(0, 5);
+        return userRepository.rankingByTotalGlory(topFive);
+    }
+
+    public List<Object> rankingByTotalKills() {
+        Pageable topFive = PageRequest.of(0, 5);
+        return userRepository.rankingByTotalKills(topFive);
     }
 }
