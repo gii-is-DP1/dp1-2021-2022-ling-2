fcab87d688b2c92de435e09213323de55ac2e114
andrsdt <andresdt@pm.me>
2022-01-20 13:41:13 +0100

    fix: adapt tests to csrf()

--- spring-boot/src/test/java/org/springframework/ntfh/playablecard/marketcard/MarketCardIngameControllerTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/playablecard/marketcard/MarketCardIngameControllerTest.java
@@ -3,17 +3,17 @@ package org.springframework.ntfh.playablecard.marketcard;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import javax.sql.DataSource;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.ntfh.entity.character.CharacterService;
 import org.springframework.ntfh.entity.game.Game;
 import org.springframework.ntfh.entity.playablecard.marketcard.ingame.MarketCardIngameController;
@@ -38,14 +38,14 @@ class MarketCardIngameControllerTest {
     private UserService userService;
 
     @Autowired
-	MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @MockBean
-	DataSource dataSource;
+    DataSource dataSource;
 
     @BeforeEach
-    void setup(){
-        Game game1= new Game();
+    void setup() {
+        Game game1 = new Game();
         game1.setId(1);
 
         when(marketCardIngameService.buyMarketCard(anyInt(), anyString())).thenReturn(game1);
@@ -54,13 +54,13 @@ class MarketCardIngameControllerTest {
 
     @Test
     @WithMockUser("user1")
-    void buyCard_success() throws Exception{
-        final String POST_JSON=
-            "{}";
-        mockMvc.perform(post("/market-cards/buy/1")
-        .header("authorization","Bearer "+TokenUtils.USER_TOKEN)
-        .content(POST_JSON)).andExpect(status().isOk());
-        
+    void buyCard_success() throws Exception {
+        final String POST_JSON = "{}";
+        mockMvc.perform(
+                post("/market-cards/buy/1").with(csrf()).header("authorization", "Bearer " + TokenUtils.USER_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON).content(POST_JSON))
+                .andExpect(status().isOk());
+
 
     }
 
