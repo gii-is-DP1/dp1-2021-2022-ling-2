ebe4ead0b689cd309de3b838f25de7060dce407a
andrsdt <andresdt@pm.me>
2022-01-20 17:04:50 +0100

    refactor: semicolon

--- spring-boot/src/main/java/org/springframework/ntfh/configuration/WebConfig.java
+++ spring-boot/src/main/java/org/springframework/ntfh/configuration/WebConfig.java
@@ -23,4 +23,4 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
     }
-};
+}
