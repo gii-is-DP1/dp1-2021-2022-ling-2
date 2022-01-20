0b9669a64dfbb76a268e074c36d518e4d117665c
Pablosancval <pablosancval@gmail.com>
2022-01-20 12:26:59 +0100

    added the first 2 cards to tests (ranger)

--- spring-boot/src/test/java/org/springframework/ntfh/cardtest/RangerCardTest.java
+++ spring-boot/src/test/java/org/springframework/ntfh/cardtest/RangerCardTest.java
@@ -1,8 +1,10 @@
 package org.springframework.ntfh.cardtest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,29 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.ntfh.command.AttackPhaseEndCommand;
-import org.springframework.ntfh.command.ChangeEnemyCommand;
-import org.springframework.ntfh.command.DealDamageCommand;
-import org.springframework.ntfh.command.DiscardCommand;
-import org.springframework.ntfh.command.DrawCommand;
-import org.springframework.ntfh.command.ExileCommand;
-import org.springframework.ntfh.command.GiveGloryCommand;
-import org.springframework.ntfh.command.GiveGoldCommand;
-import org.springframework.ntfh.command.GiveGuardCommand;
-import org.springframework.ntfh.command.GiveWoundCommand;
-import org.springframework.ntfh.command.GoldOnKillCommand;
-import org.springframework.ntfh.command.HandToAbilityPileCommand;
-import org.springframework.ntfh.command.HealCommand;
-import org.springframework.ntfh.command.ReceiveDamageCommand;
-import org.springframework.ntfh.command.RecoverCardCommand;
-import org.springframework.ntfh.command.RecoverCommand;
-import org.springframework.ntfh.command.RestrainCommand;
-import org.springframework.ntfh.command.StealCoinCommand;
 import org.springframework.ntfh.entity.character.Character;
 import org.springframework.ntfh.entity.character.CharacterService;
 import org.springframework.ntfh.entity.enemy.Enemy;
 import org.springframework.ntfh.entity.enemy.EnemyService;
-import org.springframework.ntfh.entity.enemy.EnemyType;
 import org.springframework.ntfh.entity.enemy.ingame.EnemyIngame;
 import org.springframework.ntfh.entity.enemy.ingame.EnemyIngameService;
 import org.springframework.ntfh.entity.game.Game;
@@ -40,11 +23,12 @@ import org.springframework.ntfh.entity.game.GameService;
 import org.springframework.ntfh.entity.game.GameStateType;
 import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCard;
 import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCardService;
-import org.springframework.ntfh.entity.playablecard.abilitycard.AbilityCardTypeEnum;
 import org.springframework.ntfh.entity.playablecard.abilitycard.ingame.AbilityCardIngame;
 import org.springframework.ntfh.entity.playablecard.abilitycard.ingame.AbilityCardIngameService;
 import org.springframework.ntfh.entity.player.Player;
 import org.springframework.ntfh.entity.turn.Turn;
+import org.springframework.ntfh.entity.turn.TurnService;
+import org.springframework.ntfh.entity.turn.TurnState;
 import org.springframework.ntfh.entity.turn.TurnStateType;
 import org.springframework.ntfh.entity.user.User;
 import org.springframework.ntfh.entity.user.UserService;
@@ -63,6 +47,9 @@ public class RangerCardTest {
     
     @Autowired
 	private GameService gameService;
+
+	@Autowired
+	private TurnService turnService;
 
 	@Autowired
 	private UserService userService;
@@ -90,9 +77,10 @@ public class RangerCardTest {
 
 	protected EnemyIngame enemyIngame;
 
+	protected EnemyIngame berserkerIngame;
+
 	@BeforeEach
 	public void init() {
-		// TODO copypaste from gameService test except the "ranger, rogue" variables
 		gameTester = new Game();
 		gameTester.setName("test game");
 		gameTester.setHasScenes(false);
@@ -119,11 +107,51 @@ public class RangerCardTest {
 		gameService.startGame(gameTester.getId());
 		Enemy SLINGER = enemyService.findEnemyById(12).get();
 		enemyIngame = enemyIngameService.createFromEnemy(SLINGER, gameTester);
+        Enemy BERSERKER = enemyService.findEnemyById(15).get();
+        berserkerIngame = enemyIngameService.createFromEnemy(BERSERKER, gameTester);
 	}
 
 	@AfterEach
 	public void teardown() {
 		gameService.delete(gameTester);
 	}
+
+	@Test
+	void testCompaneroLobo(){
+		
+		AbilityCard companeroLobo = abilityCardService.findById(1);
+        AbilityCardIngame abilityCardIngameRanger =
+                abilityCardIngameService.createFromAbilityCard(companeroLobo, ranger);
+        String tokenRanger = TokenUtils.generateJWTToken(ranger.getUser());
+        List<AbilityCardIngame> hand = new ArrayList<>();
+        hand.add(abilityCardIngameRanger);
+        ranger.setHand(hand);
+        abilityCardIngameService.playCard(abilityCardIngameRanger.getId(),
+                berserkerIngame.getId(), tokenRanger);
+        
+        assertThat(berserkerIngame.getCurrentEndurance()).isEqualTo(4);
+		assertThat(ranger.getGuard()).isEqualTo(2);
+	}
     
+	@Test
+	void disparoCertero(){
+
+		Integer gameId = gameTester.getId();
+		Turn initialTurn = gameService.getCurrentTurnByGameId(gameId);
+		TurnStateType initialTurnState = initialTurn.getStateType();
+
+		AbilityCard disparoCertero = abilityCardService.findById(2);
+        AbilityCardIngame abilityCardIngameRanger =
+                abilityCardIngameService.createFromAbilityCard(disparoCertero, ranger);
+        String tokenRanger = TokenUtils.generateJWTToken(ranger.getUser());
+        List<AbilityCardIngame> hand = new ArrayList<>();
+        hand.add(abilityCardIngameRanger);
+        ranger.setHand(hand);
+        abilityCardIngameService.playCard(abilityCardIngameRanger.getId(),
+                berserkerIngame.getId(), tokenRanger);
+        
+        assertThat(berserkerIngame.getCurrentEndurance()).isEqualTo(3);
+		assertThat(initialTurn.getStateType()).isNotEqualTo(initialTurnState); //the game has changed from player phase to market phase
+		
+	}
 }
